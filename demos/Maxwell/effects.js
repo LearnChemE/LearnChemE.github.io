462dab46d52569777eaa8c825b22d9988c8309da
Neil Hendren <neil.hendren@gmail.com>
2019-10-22 17:29:01 -0600

    this is difficult

--- demos/Maxwell/effects.js
+++ demos/Maxwell/effects.js
@@ -1,9 +1,14 @@
 var glowList = [];
+let running = false;
+let prg = 0;
+let x1, x2, y1, y2;
 
 function applyEffect(a) {
+
     for(i = 0; i < a.length; i++) {
         let t = a[i]['target'];
         let f = a[i]['effect'];
+        let col = a[i]['color'] || null;
         /* option to specify between effects on DOM object or canvas object. Defaults to DOM. */
         let domQ = (a[i]['type'] || "DOM") == "cnvElement" ? false : true;
         
@@ -13,7 +18,6 @@ function applyEffect(a) {
                     t.style.opacity = 1;
                     t.style.transition = "opacity 0.5s";
                     t.style.opacity = 0;
-                    effects = false;
                     break;
                 case 'expand':
                     t.style.fontSize = fontSize;
@@ -21,29 +25,70 @@ function applyEffect(a) {
                     t.firstChild.style.transition = "all 1s";
                     t.firstChild.style.fontSize = `${fontSize * 2.5 / 10}rem`;
                     t.style.top = `${10 + 2 * lnHt}px`;
-                    effects = false;
                     break;
                 case 'glow':
                     glow({
+                        continuous: page == 2 ? true : false,
                         duration: 2000,  
                         element: t,
                         id: Math.floor(i / 2)
                     });
-                    effects = true;
+                    break;
+                case 'color':
+                    glow({
+                        continuous: false,
+                        duration: 1000,
+                        element: t,
+                        id: col
+                    });
+                    break;
+                case 'zeroAndDelete':
+                    if(!running) {
+                        prg = 0;
+                        x1 = t[0].getClientRects()[0]["left"];
+                        x2 = t[1].getClientRects()[0]["right"];
+                        y1 = t[0].getClientRects()[0]["bottom"] + 25;
+                        y2 = t[1].getClientRects()[0]["top"] - 25;
+                        running = true;
+                        effects = true;
+                        update = false;
+                        for(i = 0; i < domObjs.length; i++) {
+                            domObjs[i].div.style('z-index', '-1');
+                        }
+                        loop();
+                    } else if(running && prg < 0.5) {
+                        prg += 0.025;
+                        let dur = 0.5;
+                        push();
+                        strokeWeight(4);
+                        line(x1, y1, (1-(prg/dur))*x1+(prg/dur)*x2, (1-(prg/dur))*y1+(prg/dur)*y2);
+                        pop();
+                    } else if(running && prg < 1) {
+                        prg += 0.025;
+                        let dur = 0.5;
+                        push();
+                        noStroke();
+                        fill(`rgba(0, 0, 0, ${(prg-dur)/dur})`);
+                        text("0", x2, y2 - lnHt);
+                        pop();
+                    } else if(running && prg >= 1) {
+                        running = false;
+                        effects = false;
+                        prg = 0;
+                        noLoop();
+                    }
                     break;
             }            
         }
     }
-    
-    setTimeout(function(){ update = true; }, 1000);
 }
 
-function glow({duration, element, id}) {
+function glow({continuous, duration, element, id}) {
     let frames = Math.floor(duration * fps/ 1000) ;
     let prog = (frameCount % frames) / frames;
-    let x = 0.5 + 0.5*Math.sin(prog*Math.PI);
+    let x = continuous ? 0.5 + 0.5*Math.sin(prog*Math.PI) : 1;
     let rgb = id == 0 ? [255, 100, 100] : id == 1 ? [100, 255, 100] : id == 2 ? [100, 100, 255] : [50, 128, 128];
     let colr = `rgb(${rgb[0]*x},${rgb[1]*x},${rgb[2]*x})`;
     element.style.color = colr;
-    if(window["lalala"] == true) {console.log(colr); window["lalala"] = false;}
+    if(continuous) {loop()};
 }
--- demos/Maxwell/main.js
+++ demos/Maxwell/main.js
@@ -1,18 +1,24 @@
-let choice, w, h, lnHt, fontSize, i;
+let w, h, lnHt, fontSize, i, constant, constantIndex, restart;
 let eqns = {
     'fundamentals':{"U":`dU=TdS-pdV`,"F":`dF=-SdT-pdV`,"H":`dH=TdS+Vdp`,"G":`dG=-SdT+Vdp`}
 };
+let choices = [];
 let domObjs = [];
 let page = 1;
 let update = false;
 let effects = false;
 let effect = [];
 let letters = [];
+let glowLetters = [];
 let fps = 20;
+let partials = [];
 
 function setup() {
     let cnv = createCanvas(windowWidth, windowHeight);
     cnv.parent('main');
+    cnv.position(0, 0);
+    cnv.style('z-index', 0);
+
     frameRate(fps);
     /* define globals relative height and width (%) of canvas */
     window["wdRelative"] = windowWidth / 100;
@@ -21,28 +27,42 @@ function setup() {
     w = wdRelative;
     h = htRelative;
 
+    let c = document.getElementById('main');
+    let b = document.createElement('button');
+    b.id = 'resetbutton'; b.innerText = 'reset'; b.onclick = reset; b.style.zIndex = 2; b.style.position ='absolute'; b.style.left = `${50*w}px`; b.style.top = '10px';
+    c.appendChild(b);
+
+    restart = createButton('click me');
+    restart.position(100*w - 50, 10);
+    restart.mousePressed(reset);
+    restart.show();
+
     /* default MathJax initialization */
     MathJax.startup.defaultReady();
 
     textAlign(LEFT, TOP);
 
     windowResized();
-    background(180);
+    background(255, 255, 255);
+    noLoop();
 }
 
 function draw() {
     if(update) {drawPage(page)};
     if(effects) {applyEffect(effect)};
+    update = false;
 }
 
 function drawPage(p) {
     
-    background(180);
+    clear();
 
     switch(p) {
         case 1:
             removeElements();
+            choices = [];
             domObjs = [];
+            effect = [];
             text("Click an equation to get started.", 10, 10);
             let k = Object.keys(eqns['fundamentals']);
             for(i = 0; i < k.length; i++) {
@@ -50,31 +70,40 @@ function drawPage(p) {
                 domObjs[i].div.addClass('eq');
                 domObjs[i].div.id(`eq${i}`);
                 let id = `${i}`;
-                document.getElementById(`eq${id}`).addEventListener("mousedown", () => {choice = parseInt(id); next(1, choice)});
+                document.getElementById(`eq${id}`).addEventListener("mousedown", () => {choices.push(parseInt(id)); next(1, choices[0])});
             }
             break;
         case 2:
             text("Click the differential term you would like held constant", 10, 10);
             for(i = 0; i < domObjs.length; i++) {
-                if (i != choice) {domObjs[i].div.remove()}
+                if (i != choices[0]) {domObjs[i].div.remove()}
             }
             effect = [];
-            letters = document.querySelectorAll('g');
-            let glowLetters = choice == 0 || choice == 2 ? [1,2,5,6,9,10] : choice == 1 || choice == 3 ? [1,2,6,7,10,11] : [];
+            //console.log(document.querySelectorAll('mjx-container')[0].firstChild.classList.value);
+            letters = document.querySelectorAll('mjx-container')[0].firstChild.childNodes;
+            glowLetters = choices[0] == 0 || choices[0] == 2 ? [0,1,4,5,8,9] : choices[0] == 1 || choices[0] == 3 ? [0,1,5,6,9,10] : [];
             for(i = 0; i < glowLetters.length; i++) {
-                let element = letters[glowLetters[i] + 1];
+                let element = letters[glowLetters[i]];
                 effect.push({"target":element, "effect":"glow"});
+                let id = `${Math.floor(i/2)}`;
+                element.addEventListener("mousedown", () => {next(2, id)});
             }
             effects = true;
             break;
         case 3:
+            domObjs.push(new Tex({"content":`(\\frac{${partials[0]}}{${partials[1]}})_{${constant}}`,"position":[20, 10 + 4 * lnHt], "name":"partial"}));
+            domObjs[domObjs.length - 1].div.hide();
+            for(i = 0; i < effect.length; i++) {effect[i]["effect"] = "color";effect[i]["color"] = Math.floor(i/2)}
+            effect.push({"target":[letters[constantIndex], letters[constantIndex + 1]], "effect":"zeroAndDelete"});
+            effects = true;
+            loop();
+            //effect.push({"target":letters[3], "effect":"color", "color":choices[1]});
             break;
         case 4:
             break;
         default:
             break;
     }
-
     MathJax.typeset();
     update = false;
 }
@@ -90,12 +119,32 @@ function next(p, c) {
                     effect.push({"target":element, "effect":event});
                 }
                 /* domObjs[i].div.remove(); */
-                update = false;
                 effects = true;
+                redraw();
+                update = true;
+                page++;
+                break;
+            case 2:
+                choices.push(c);
+                let k = Object.keys(eqns['fundamentals']);
+                let str = eqns['fundamentals'][k[choices[0]]];
+                constant = str.charAt(glowLetters[2*c+1]);
+                constantIndex = glowLetters[2 * c];
+                glowLetters.splice(2*c, 2);
+                partials = [];
+                for(i = 0; i < glowLetters.length / 2; i++) {
+                    partials.push(str.substring(glowLetters[2*i], glowLetters[2*i+1] + 1))
+                }
+                update = true;
                 page++;
                 break;
         }
+        redraw();
     }
+}
+
+function reset() {
+    location.reload();
 }
 
 function windowResized() {
