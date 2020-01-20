1c11200c82e8c25192a635a6942b9d237e069b93
Neil Hendren <neil.hendren@gmail.com>
2020-01-20 15:57:23 -0700

    isothermal reactor initial commit

--- demos/IsothermalPlugFlowReactor/main.js
+++ demos/IsothermalPlugFlowReactor/main.js
@@ -10,21 +10,22 @@ var aspRatio = 1;
 var clientHeight = clientWidth * aspRatio;
 
 var gui;
-var T = 450;
-var tfinal = 15;
-var dt = 0.1;
+var vfinal = 5;
+var dv = 0.1;
 var mainPlot;
 var equation;
 
-var CA;
-var CB;
-var CC;
-var Cao = 2;
-var Cbo = 0;
-var Cco = 0;
-var selectivity;
+var FA;
+var FB;
+var FC;
+var xA0;
+var k;
+var u = 1;
+var n;
 
-var page = "concentration";
+var title = `\\(\\mathrm{ A+2B \\rightarrow C \\qquad\\qquad } r = 0.50 \\mathrm{ C_{A}C_{B} \\;  \\frac{mol}{m^{3}s} } \\)`;
+var titleDOM;
+let mjready = false;
 
 const sim = math.parser();
 
@@ -37,53 +38,94 @@ const sim = math.parser();
  */
 function updateData() {
   // set initial conditions and constants
-  sim.eval("Ea1 = 145");
-  sim.eval("Ea2 = 155");
-  sim.eval("R  = 8.314*10^-3");
-  sim.eval("A1 = 3.6*10^16");
-  sim.eval("A2 = 1.8*10^17");
-  sim.eval(`tfinal = ${tfinal}`);
-  sim.eval(`dt = ${dt}`);
+  sim.eval(`Ca0 = ${xA0}`);
+  sim.eval(`Cb0 = ${1 - xA0}`);
+  sim.eval(`Cc0 = 0`);
+  sim.eval(`k = ${k}`);
+  sim.eval(`u = ${u}`);
+  sim.eval(`n = ${n}`);
 
-  sim.eval(`Ca0 = ${Cao}`);
-  sim.eval(`Cb0 = ${Cbo}`);
-  sim.eval(`Cc0 = ${Cco}`);
-
-  sim.eval(`k1 = A1 * e^(-Ea1/(R * ${T}))`);
-  sim.eval(`k2 = A2 * e^(-Ea2/(R * ${T}))`);
+  sim.eval(`vfinal = ${vfinal}`);
+  sim.eval(`dv = ${dv}`);
 
   // define differential equations
-  sim.eval("dCadt(Ca, Cb, Cc) = -k1 * Ca");
-  sim.eval("dCbdt(Ca, Cb, Cc) = k1 * Ca - k2 * Cb");
-  sim.eval("dCcdt(Ca, Cb, Cc) = k2 * Cb");
+  sim.eval("dCadv(Ca, Cb, Cc) = -k * Ca * (Cb ^ n) / u");
+  sim.eval("dCbdv(Ca, Cb, Cc) = -2 * k * Ca * (Cb ^ n) / u");
+  sim.eval("dCcdv(Ca, Cb, Cc) = k * Ca * (Cb ^ n) / u");
 
   // evaluate system of ODEs and create an array from the result
-  sim.eval("result = ndsolve([dCadt, dCbdt, dCcdt], [Ca0, Cb0, Cc0], dt, tfinal)");
+  sim.eval("result = ndsolve([dCadv, dCbdv, dCcdv], [Ca0, Cb0, Cc0], dv, vfinal)");
 
-  const Ca = sim.eval("transpose(concat(transpose(result[:,1]),1))").toArray().map(function(e) { return e[0];});
-  const Cb = sim.eval("transpose(concat(transpose(result[:,2]),1))").toArray().map(function(e) { return e[0];});
-  const Cc = sim.eval("transpose(concat(transpose(result[:,3]),1))").toArray().map(function(e) { return e[0];});
+  let Ca = sim.eval("transpose(concat(transpose(result[:,1]),1))").toArray().map(function(e) { return e[0];});
+  let Cb = sim.eval("transpose(concat(transpose(result[:,2]),1))").toArray().map(function(e) { return e[0];});
+  let Cc = sim.eval("transpose(concat(transpose(result[:,3]),1))").toArray().map(function(e) { return e[0];});
+
+  let zeroIndex = -1;
+  let CaCbZero;
+  let tempCa, tempCb, tempCc;
+
+  for(let i = 0; i < Ca.length; i++) {
+    if(Ca[i] <= 0) {
+      zeroIndex = i;
+      CaCbZero = "Ca";
+      break;
+    }
+    if(Cb[i] <= 0) {
+      zeroIndex = i;
+      CaCbZero = "Cb";
+      break;
+    }
+  }
+
+  if(zeroIndex >= 0) {
+    sim.eval(`Ca0 = ${CaCbZero == "Ca" ? 0 : xA0 - ((1 - xA0) / 2)}`);
+    sim.eval(`Cb0 = ${CaCbZero == "Cb" ? 0 : (1 - 2 * xA0)}`);
+    sim.eval(`Cc0 = ${CaCbZero == "Ca" ? xA0 : ((1 - xA0) / 2)}`);
+    sim.eval(`k = ${k}`);
+    sim.eval(`u = ${u}`);
+    sim.eval(`n = ${n}`);
+  
+    sim.eval(`vfinal = ${vfinal * (1 - (zeroIndex / Ca.length))}`);
+    sim.eval(`dv = ${dv}`);
+  
+    // define differential equations
+    sim.eval(`dCadv(Ca, Cb, Cc) = 0`);
+    sim.eval(`dCbdv(Ca, Cb, Cc) = 0`);
+    sim.eval(`dCcdv(Ca, Cb, Cc) = 0`);
+  
+    // evaluate system of ODEs and create an array from the result
+    sim.eval("result = ndsolve([dCadv, dCbdv, dCcdv], [Ca0, Cb0, Cc0], dv, vfinal)");
+  
+    tempCa = sim.eval("transpose(concat(transpose(result[:,1]),1))").toArray().map(function(e) { return e[0];});
+    tempCb = sim.eval("transpose(concat(transpose(result[:,2]),1))").toArray().map(function(e) { return e[0];});
+    tempCc = sim.eval("transpose(concat(transpose(result[:,3]),1))").toArray().map(function(e) { return e[0];});
+  
+    for(let i = 0; i < tempCa.length; i++) {
+      Ca[zeroIndex + i] = tempCa[i];
+      Cb[zeroIndex + i] = tempCb[i];
+      Cc[zeroIndex + i] = tempCc[i];
+    }
+  }
 
   let l = Ca.length;
 
-  CA = new Array(l);
-  CB = new Array(l);
-  CC = new Array(l);
-  selectivity = new Array(l);
+  FA = new Array(l);
+  FB = new Array(l);
+  FC = new Array(l);
 
-  for (i=0; i<Ca.length;i++) {
-    let t = (tfinal/l)*(i+1);
-    CA[i]=[t, Ca[i]];
-    CB[i]=[t, Cb[i]];
-    CC[i]=[t, Cc[i]];
-    selectivity[i]=[t, CB[i][1]/CC[i][1]];
+  for (i = 0; i < FA.length; i++) {
+    let v = (vfinal/l) * (i + 1);
+    FA[i]=[v, Ca[i] * u];
+    FB[i]=[v, Cb[i] * u];
+    FC[i]=[v, Cc[i] * u];
   }
 
   // add initial conditions to beginning of array. Initial selectivity is technically infinite.
-  CA.unshift([0,Cao]);
-  CB.unshift([0,Cbo]);
-  CC.unshift([0,Cco]);
-  selectivity.unshift([0, 100]);
+  FA.unshift([0, xA0 * u]);
+  FB.unshift([0, (1 - xA0) * u]);
+  FC.unshift([0, 0]);
+
+
 }
 
 /**
@@ -110,38 +152,49 @@ function windowResized() {
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 function setup() {
+  titleDOM = document.getElementById('title');
   let cnv = createCanvas(clientWidth, clientHeight);
   cnv.parent('plotContainer');
   // Declare a plot
   mainPlot = new PlotCanvas(this);
-  mainPlot.xLims = [0, tfinal];
-  mainPlot.yLims = [0, 2.5];
-  mainPlot.xAxisLabel = "time (h)";
-  mainPlot.yAxisLabel = " ";
+  mainPlot.xLims = [0, vfinal];
+  mainPlot.yLims = [0, 1];
+  mainPlot.xAxisLabel = "volume (m³)";
+  mainPlot.yAxisLabel = "molar flow rate (mol / s)";
   mainPlot.plotTitle = "";
   mainPlot.plotSetup();
   
   // Create the GUI using p5.gui.js
   gui = createGui('Plot Controls', clientWidth - 10, mainPlot.GPLOT.mar[2] + 100);
-  gui.newSlider('T', 450, 480, 450, 1, 'Temperature', 'K');
-  gui.newDropdown("page", ["concentrations", "selectivity"], "Display:");
+  gui.newSlider('xA0', 0, 1, 0.5, 0.01, 'inlet mole fraction A');
+  gui.newSlider('k', 0, 1, 0.5, 0.01, 'reaction rate constant <i>k</i>','<span id="kunits">L / mol</span>');
+  gui.newSlider('n', 0, 2, 1, 1, 'reaction order of B');
   updateData();
 
-  CaFunction = new ArrayPlot(CA);
-  CaFunction.lineColor = color(255, 0, 0, 255);
+  FaFunction = new ArrayPlot(FA);
+  FaFunction.lineColor = color(255, 0, 0, 255);
 
-  CbFunction = new ArrayPlot(CB);
-  CbFunction.lineColor = color(50, 220, 0, 255);
+  FbFunction = new ArrayPlot(FB);
+  FbFunction.lineColor = color(50, 220, 0, 255);
 
-  CcFunction = new ArrayPlot(CC);
-  CcFunction.lineColor = color(0, 0, 255, 255);
+  FcFunction = new ArrayPlot(FC);
+  FcFunction.lineColor = color(0, 0, 255, 255);
 
-  selFunction = new ArrayPlot(selectivity);
-  selFunction.lineColor = color(0, 0, 0, 0);
+  mainPlot.addFuncs(FaFunction, FbFunction, FcFunction);
 
-  //secondaryFunction = new Plot(equation, "x", 0, 1);
+  let delay = 1000;
+  setTimeout(function jax() {
+      try {titleDOM.innerText = title; MathJax.Hub.Configured(); MathJax.Hub.Register.StartupHook("End",function () { mjready = true });}
+      catch(error) {
+        console.error(error); if(delay < 10000) {
+          delay*= 1.5; setTimeout(jax, delay);
+        } else {
+          console.log("mathjax could not load");
+      }}
+  }, delay);
 
-  mainPlot.addFuncs(CaFunction, CbFunction, CcFunction, selFunction);
+  let GUIDOM = document.getElementsByClassName("qs_main")[0];
+  GUIDOM.addEventListener("mouseup", e => {updateJax()});
 
   noLoop();
 }
@@ -156,64 +209,38 @@ function draw() {
   
   // updates the solution, then updates the lines on the plot
   updateData();
-  CaFunction.update(CA);
-  CbFunction.update(CB);
-  CcFunction.update(CC);
-  selFunction.update(selectivity);
+  FaFunction.update(FA);
+  FbFunction.update(FB);
+  FcFunction.update(FC);
 
   // hides/shows lines depending on page selected
-  switch(page) {
-    case "selectivity":
-      mainPlot.yAxisLabel = "selectivity (     /     )";
-      mainPlot.GPLOT.getYAxis().getAxisLabel().setText(mainPlot.yAxisLabel);
-      selFunction.lineColor = color(0, 0, 0, 255);
-      CaFunction.lineColor = color(255, 0, 0, 0);
-      CbFunction.lineColor = color(50, 220, 0, 0);
-      CcFunction.lineColor = color(0, 0, 255, 0);
-      break;
-    default:
-      mainPlot.yAxisLabel = "concentration (mol / L)";
-      mainPlot.GPLOT.getYAxis().getAxisLabel().setText(mainPlot.yAxisLabel);
-      selFunction.lineColor = color(0, 0, 0, 0);
-      CaFunction.lineColor = color(255, 0, 0, 255);
-      CbFunction.lineColor = color(50, 220, 0, 255);
-      CcFunction.lineColor = color(0, 0, 255, 255);
-    break;
-  }
+  FaFunction.lineColor = color(255, 0, 0, 255);
+  FbFunction.lineColor = color(50, 220, 0, 255);
+  FcFunction.lineColor = color(0, 0, 255, 255);
 
   mainPlot.plotDraw();
-  let selX = tfinal / 2;
-  let selY = selectivity[Math.trunc(map(tfinal/2, 0, tfinal, 0, tfinal / dt - 1))][1] + 0.1;
-  let CAX = tfinal / 6;
-  let CAY = CA[Math.trunc(map(tfinal/6, 0, tfinal, 0, tfinal / dt - 1))][1];
-  let CBX = tfinal / 2;
-  let CBY = CB[Math.trunc(map(tfinal/2, 0, tfinal, 0, tfinal / dt - 1))][1];
-  let CCX = 5 * tfinal / 6;
-  let CCY = CC[Math.trunc(map(5*tfinal/6, 0, tfinal, 0, tfinal / dt - 1))][1];
-
-  mainPlot.labelDraw("selectivity", selX, selY, selFunction.lineColor, [CENTER, CENTER], false, [5, 1.6]);
+  let FAX = vfinal / 6;
+  let FAY = FA[Math.trunc(map(vfinal/6, 0, vfinal, 0, vfinal / dv - 1))][1];
+  let FBX = vfinal / 2;
+  let FBY = FB[Math.trunc(map(vfinal/2, 0, vfinal, 0, vfinal / dv - 1))][1];
+  let FCX = 5 * vfinal / 6;
+  let FCY = FC[Math.trunc(map(5*vfinal/6, 0, vfinal, 0, vfinal / dv - 1))][1];
  
-  mainPlot.labelDraw("C", CAX, CAY, CaFunction.lineColor, [RIGHT, CENTER], false, [2, 1.8]);
-  mainPlot.labelDraw("A", CAX, CAY, CaFunction.lineColor, [LEFT, TOP],"sub");
+  mainPlot.labelDraw("F", FAX, FAY, FaFunction.lineColor, [RIGHT, CENTER], false, [2, 1.8]);
+  mainPlot.labelDraw("A", FAX, FAY, FaFunction.lineColor, [LEFT, TOP],"sub");
   
-  mainPlot.labelDraw("C", CBX, CBY, CbFunction.lineColor, [RIGHT, CENTER], false, [2, 1.8]);
-  mainPlot.labelDraw("B", CBX, CBY, CbFunction.lineColor, [LEFT, TOP],"sub");
+  mainPlot.labelDraw("F", FBX, FBY, FbFunction.lineColor, [RIGHT, CENTER], false, [2, 1.8]);
+  mainPlot.labelDraw("B", FBX, FBY, FbFunction.lineColor, [LEFT, TOP],"sub");
  
-  mainPlot.labelDraw("C", CCX, CCY, CcFunction.lineColor, [RIGHT, CENTER], false, [2, 1.8]);
-  mainPlot.labelDraw("C", CCX, CCY, CcFunction.lineColor, [LEFT, TOP],"sub");
-
-  push();
-  translate(mainPlot.GPLOT.pos[0] + mainPlot.GPLOT.mar[1], mainPlot.GPLOT.pos[1] + mainPlot.GPLOT.mar[2] + mainPlot.GPLOT.dim[1]);
-  fill(mainPlot.GPLOT.boxBgColor.levels[0], mainPlot.GPLOT.boxBgColor.levels[1], mainPlot.GPLOT.boxBgColor.levels[2], selFunction.lineColor._array[3]*255);
-  noStroke();
-  rectMode(CENTER);
-  rect(mainPlot.GPLOT.mainLayer.valueToXPlot(selX) + mainPlot.fontSize*3.3, mainPlot.GPLOT.mainLayer.valueToYPlot(selY), 30, 55);
-  pop();
+  mainPlot.labelDraw("F", FCX, FCY, FcFunction.lineColor, [RIGHT, CENTER], false, [2, 1.8]);
+  mainPlot.labelDraw("C", FCX, FCY, FcFunction.lineColor, [LEFT, TOP],"sub");
   
-  mainPlot.subSuperDraw("C","B", selX, selY, selFunction.lineColor, "sub", 0, 3.3*mainPlot.fontSize, -0.9*mainPlot.fontSize);
-  mainPlot.subSuperDraw("C","C", selX, selY, selFunction.lineColor, "sub", 0, 3.3*mainPlot.fontSize, 0.8*mainPlot.fontSize);
-  mainPlot.drawLine(selX, selY, mainPlot.fontSize*3, 0, selFunction.lineColor, 1, mainPlot.fontSize*4, 0);
+}
 
-  mainPlot.yaxisSubSuperDraw("C","B", 0, 1.4, selFunction.lineColor, "sub", -PI/2);
-  mainPlot.yaxisSubSuperDraw("C","C", 0, 3.2, selFunction.lineColor, "sub", -PI/2);
+function updateJax() {
+  if(mjready) {
+    title = `\\mathrm{ A+2B \\rightarrow C \\qquad\\qquad } r = ${k} \\mathrm{ C_{A}C_{B}${n != 1 ? String("^{" + n + "}") : ""} \\;  \\frac{mol}{m^{3}s} } `;
+    var math = MathJax.Hub.getAllJax("title")[0];
+    MathJax.Hub.Queue(["Text", math, title]);
+  }
 }
