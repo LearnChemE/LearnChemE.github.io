37a092d47386a50ba78b47ec37cece65fca4331b
Neil Hendren <neil.hendren@gmail.com>
2020-07-08 15:54:55 -0600

    more time before refresh

--- lab-experiments/single_stage_distillation/_dev/compiler.js
+++ lab-experiments/single_stage_distillation/_dev/compiler.js
@@ -19,7 +19,7 @@ let liveServerParams = {
   root: path.resolve(__dirname, "../dist/"), // Set root directory that's being served. Defaults to cwd.
   open: true, // When false, it won't load your browser by default.
   file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
-  wait: 1500, // Waits for all changes, before reloading. Defaults to 0 sec.
+  wait: 3000, // Waits for all changes, before reloading. Defaults to 0 sec.
   logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
   noCssInject: true
 };
