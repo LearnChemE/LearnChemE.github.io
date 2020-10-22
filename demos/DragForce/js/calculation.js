a73d9ad2ecbae0d5e8a5e60fce960e7088551a7a
Neil Hendren <neil.hendren@gmail.com>
2020-10-21 20:44:21 -0600

    some logical fixes

--- demos/DragForce/js/calculation.js
+++ demos/DragForce/js/calculation.js
@@ -1,61 +1,44 @@
 //This JS file provides calculations for simulation
 var g = 9.8; //gravity
 var rho_air = 1.23; //air density
-var mspc = 1; //ms per calculation
-var d_t = mspc/1000;
+var time = 0;
 var Cd = 0.5;
 var A = 0.147;
 var M = 0.75;
-var max_t = 8000;
-
+var fall_height = 324; // height of eiffel tower in meters
 
 var acceleration = 0;
 var velocity = 0;
-var height = 200; 
+var height = fall_height; 
 var dragForce = 0;
-
-let VTdict = {};
-let HTdict = {};
-let FTdict = {};
-
-//Drag force function
-//Input: Drag Coefficient, Velocity,Area
-//Return: magnitude of force
-function Drag()
-{
-    dragForce = Cd*rho_air*velocity*velocity*A/2;
-    return dragForce;
-}
-
-//The acc function 
-//Input:takes mass, drag, objects position, time interal
-//Return: acceleration
-function acc()
-{
-    acceleration =  (g-Drag()/M);
-    return acceleration;
-}
 
 //Position 
 //Acceleration is also changing with time. Taking small time intervals (0.001) for calculation. 
-function V()
+function advance()
 {
-    velocity = velocity + d_t*acc();
-    height = height - velocity*d_t;
+    dragForce = Cd*rho_air*velocity*velocity*A/2;
+    acceleration = g - dragForce / M;
+    velocity = velocity + dt*acceleration;
+    height = height - dt*velocity;
+    time += dt;
     return velocity;
 }
 
-function VT()
+function VT() // calculates the total time it will take to fall and sets the global variable "fallTime" to that value.
 {
-    var time = 0; // time of falling
-    while((height >= 0) && (time <= max_t)) //Max time 10 secs
-    {
-        VTdict[time] = V();
-        HTdict[time] = height;
-        FTdict[time] = dragForce;
-        time = time + mspc;
+    initValue();
+    dt = 0.01;
+
+    while(height >= 0) { 
+        advance();
     }
-    
+
+    max_velocity = velocity;
+    fallTime = time;
+    initValue();
+
+    return fallTime;
+
 }
 
 function initValue()
@@ -64,28 +47,26 @@ function initValue()
     A = document.getElementById("area").value;
     M = document.getElementById("mass").value;
 
+    time = 0;
     acceleration = 0;
     velocity = 0;
-    height = 400;
-    VTdict[0] = velocity;
-    HTdict[0] = height;
-    FTdict[0] = Drag();
+    height = fall_height;
+    dragForce = 0;
+    time = 0;
 
     start = Date.now(); // This is the time value when the animation starts - it is updated once when the animation begins
     now = Date.now();
     elapsed = now - start;
 
-    updateData();
+    updateDOM();
 
 }
 
-function updateData()
+function updateDOM()
 {
-    document.getElementById("GForce").innerHTML = (g * M).toFixed(2);
-    document.getElementById("HVelocity").innerHTML = VTdict[elapsed].toFixed(1);
-    document.getElementById("HHeight").innerHTML = HTdict[elapsed].toFixed(0);
-    document.getElementById("HAcceleration").innerHTML = ((VTdict[elapsed]-VTdict[elapsed-1])/0.001).toFixed(3);
-    document.getElementById("HDrag").innerHTML = FTdict[elapsed].toFixed(2);
-
+    document.getElementById("GForce").innerHTML = Number(g * M).toFixed(1);
+    document.getElementById("HDrag").innerHTML = Number(dragForce).toFixed(1);
+    document.getElementById("HVelocity").innerHTML = Math.round(velocity);
+    document.getElementById("HHeight").innerHTML = Math.abs(Number(height).toFixed(0));
+    document.getElementById("HAcceleration").innerHTML = Number(acceleration).toFixed(1);
 }
-
--- demos/DragForce/js/main.js
+++ demos/DragForce/js/main.js
@@ -1,17 +1,26 @@
 // This is the main program.  I just threw all of the functions not related to initializing the plot in here
 
 // These are some elements you can find in "index.html"
-const ball = document.getElementById("ball");
-const startButton = document.getElementById("start-button");
+const objImage = document.getElementById("objImage");
+const startPauseButton = document.getElementById("start-pause-button");
+const resetButton = document.getElementById("reset-button");
 const selection = document.getElementById("object-selection");
+const cdInp = document.getElementById("dragCoeff");
+const aInp = document.getElementById("area");
+const mInp = document.getElementById("mass");
 
-let yPosition = 0; // Initial position of the ball
+let yPosition = 0; // Initial position of the objImage
 let isRunning = false; // Animation is initially not running
+let isPaused = false;
 let start = Date.now(); // This is the time value when the animation starts - it is updated once when the animation begins
 let now = Date.now(); // This is the time value during the animation - it is updated every animation frame
 let elapsed = now - start; // This is the time elapsed - the difference between "now" and "start"
 let graphData = [[0, 0]]; // This is the "graph data" variable that we will update then put into the graph
 let index = 0; // This is just a number that increments every animation frame. You may find it useful for something
+let dt = 1; //ms per calculation
+let fallTime = 10; // The amount of time it takes to fall
+let max_velocity = 50;
+let tower_image_size = 285; // see style.css
 
 // This function updates the Plot with a two-dimensional array of coordinates, e.g. [[0, 0], [5, 2], [3, 1], ...]
 // See some examples of live updating at http://www.flotcharts.org/flot/examples/ . You can view the source code of the examples in the index.html file of each example page on this website.
@@ -21,24 +30,42 @@ function updateGraph(array) {
   Plot.draw();
 }
 
+function updateAxes() {
+  const maxXAxis = Math.ceil(fallTime);
+  const maxYAxis = Math.ceil(max_velocity);
+  Plot.getOptions().xaxes[0].max = maxXAxis;
+  Plot.getOptions().yaxes[0].max = Number( 20 + maxYAxis - maxYAxis % 20); // sets y axis to max velocity in increments of 20
+  Plot.setupGrid();
+  Plot.draw();
+}
+
 // This function is called once every animation frame.
 function animationFunction() {
   index++; // Increment our arbitrary "index" variable by 1
-  graphData.push([elapsed / 1000, VTdict[elapsed]]); // Add new data to the "graphData" variable 
-  if(elapsed<=max_t)
-  {updateData();}
+  advance();
+  graphData.push([elapsed / 1000, velocity]); // Add new data to the "graphData" variable
+  updateDOM();
   updateGraph(graphData);
-  ball.style.top = `${400-HTdict[elapsed]}px`; // Change the css property "top" of the ball to make it move
+  objImage.style.top = `${ (fall_height - height) * ( tower_image_size / fall_height ) }px`; // Change the css property "top" of the objImage to make it move
 }
 
 // This is an implementation based on https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
 // This function just increments the "now" variable to the current time, calls animationFunction(), then calls itself until 5000 ms have elapsed
 function step() {
-  now = Date.now();
-  elapsed = now - start;
-  animationFunction();
+  if ( !isPaused ) {
+    dt = (Date.now() - now) / 1000;
+    if ( dt == 0 ) { dt = 0.016 }
+    now = Date.now();
+    elapsed = now - start;
+    animationFunction();
+  } else {
+    dt = (Date.now() - now) / 1000;
+    now = Date.now();
+    start += (dt * 1000);
+    elapsed = now - start;
+  }
 
-  if (elapsed < max_t && isRunning) { // Stop the animation after 5 seconds
+  if ( height > 0 && isRunning) { // Stop the animation after 5 seconds
     window.requestAnimationFrame(step);
   } else {
     isRunning = false;
@@ -48,48 +75,111 @@ function step() {
 
 // This is the function that is called when you click the "start" button
 // We do not want it to be called if the simulation is already running, hence the "isRunning" variable
-function startAnimation() {
+function startPauseAnimation() {
   
+  startPauseButton.innerHTML = "Pause"
+
   if (!isRunning) {
-    initValue();
+    [selection, cdInp, aInp, mInp].forEach(inp => { inp.setAttribute("disabled", "true") });
     VT();
+    updateAxes();
     isRunning = true;
     index = 0;
     graphData = [[0, 0]];
     start = Date.now();
     yPosition = 0;
     window.requestAnimationFrame(step);
-  }
   
+  } else {
 
+    if (isPaused) {
+      isPaused = false;
+      startPauseButton.innerHTML = "Pause"
+    } else {
+      isPaused = true;
+      startPauseButton.innerHTML = "Start"
+    }
+  
+  }
 
 }
 
-function stopAnimation()
-{
-if(isRunning)
-  {
-    window.cancelAnimationFrame(step);
-    isRunning = false;
-  }
+function resetAnimation() {
+  isRunning = false;
+  isPaused = false;
+  selection.removeAttribute("disabled");
+  window.cancelAnimationFrame(step);
+  initValue();
+  graphData = [[0, 0]];
+  fallTime = 10;
+  startPauseButton.innerHTML = "Start"
+  updateAxes();
 }
 
 function updateInput()
 {
-    if(selection.value != -1)
-    {
-        document.getElementById("dragCoeff").value = obj[selection.value]["Cd"];
-        document.getElementById("area").value= obj[selection.value]["A"];
-        document.getElementById("mass").value= obj[selection.value]["M"];
-        document.getElementById("objImage").scr = obj[selection.value]["src"];
-    }
-    if(selection.value == -1)
-    {
-        log("something");
-    }
-
+  // selection.value is a string, so make sure this switch statement uses strings
+  switch(selection.value) {
+    default:
+      FallObject.setCustom();
+      break;
+    case "1":
+      FallObject.setSoccer();
+      break;
+    case "2":
+      FallObject.setJumper();
+      break;
+    case "-1":
+      FallObject.setCustom();
+      break;
+  }
 }
 
+function fallObject() {
+
+  this.setSoccer = () => {
+    this.Cd = 0.5;
+    this.A = 0.147;
+    this.M = 0.75;
+    this.src = "../img/ball.png";
+    this.updateInputs(true);
+  }
+
+  this.setJumper = () => {
+    this.Cd = 1.4;
+    this.A = 95.03;
+    this.M = 174,
+    this.src = "../img/Parachute-Fallschirm.svg";
+    this.updateInputs(true);
+  }
+
+  this.setCustom = () => {
+    this.src = "../img/ball.png";
+    this.updateInputs(false);
+  }
+
+  this.updateInputs = (disable) => {
+    if(disable) {
+      [cdInp, aInp, mInp].forEach(inp => {
+        inp.setAttribute("disabled", "true");
+      });
+      cdInp.value = this.Cd;
+      aInp.value = this.A;
+      mInp.value = this.M;
+    } else {
+      [cdInp, aInp, mInp].forEach(inp => {
+        inp.removeAttribute("disabled");
+      });
+    }
+    objImage.setAttribute("src", this.src);
+  }
+
+  this.setSoccer();
+}
+
+let FallObject = new fallObject();
+
 // Instruct the start button to call the startAnimation() function when we click it
-startButton.addEventListener("click", startAnimation);
+startPauseButton.addEventListener("click", startPauseAnimation);
+resetButton.addEventListener("click", resetAnimation);
 selection.addEventListener("change", updateInput);
