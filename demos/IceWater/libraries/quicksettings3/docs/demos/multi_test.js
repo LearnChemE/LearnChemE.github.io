37735c4f63b6916c5e5bd960806b84ce2a888cf1
Neil Hendren <neil.hendren@gmail.com>
2021-01-19 07:17:00 -0700

    modified "about" pages

--- demos/IceWater/libraries/quicksettings3/docs/demos/multi_test.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-
-  var panel1 = QuickSettings.create(10, 10, "Panel 1")
-      .addColor("foo", "#ffff00")
-      .addBoolean("bar", false);
-  var panel1 = QuickSettings.create(220, 10, "Panel 2")
-      .addColor("foo", "#ff0000")
-      .addBoolean("bar", false);
-}
