453232aa794a5d11650ee1cbd4110ef299d708f5
Alex Jimenez <alji1305@colorado.edu>
2022-03-29 21:25:26 -0600

    Comment 3 edited

--- demos/AlexPracticeTemplate/index.js
+++ demos/AlexPracticeTemplate/index.js
@@ -1,10 +1,10 @@
 // Declare global variables within this object. They will be available across all files as "g.variable_name".You can make another script file aside from index.js by putting <script src="./path-to/other-js-file.js"></script> after the "index.js" HTML element. All the variables you declare in this file will be accessible there. It's best practice to store your global variables within an object E.G. "g.rng_1_value" because it will minimize the risk of namespace issues.
 window.g = {
-  cnv : undefined,
-  rng_1_value : 0,
-  rng_2_value : 0,
-  rng_3_value : 0,
-  select_value : "value-1",
+  cnv: undefined,
+  rng_1_value: 0,
+  rng_2_value: 0,
+  rng_3_value: 0,
+  select_value: "value-1",
 }
 
 // See https://p5js.org/ to learn how to use this graphics library. setup() and draw() are used to draw on the canvas object of the page.  Seriously, spend some time learning p5.js because it will make drawing graphics a lot easier.  You can watch tutorial videos on the "Coding Train" youtube channel. They have a p5.js crash course under their playlists section.  It will make these functions make a lot more sense.
@@ -24,14 +24,14 @@ function draw() {
   background(250);
 
   push();
-  
+
   let step = frameCount % 600; // Modulus operator limits step variable to between 0 and 600.
 
   // An example of how to draw a graphic with the P5.js library.  This statement draws a line that moves across the canvas every 10 seconds (10000 ms). Recall that 16.67 ms * 600 = 10000 ms or 10 seconds.
 
-  stroke(255, 100, 100); // Color of the line
+  stroke(255, 100, 200); // Color of the line across screen
   strokeWeight(3); // Width of the line
-   // "line" syntax: line(x1, y2, x2, y2)
+  // "line" syntax: line(x1, y2, x2, y2)
   line(
     (step / 600) * width,
     height / 2 - 150,
@@ -42,6 +42,7 @@ function draw() {
   pop();
 }
 
+// connects html to Javascript and assigns them to a const variable
 const range_1_element = document.getElementById("range-1");
 const range_1_value_label = document.getElementById("range-1-value");
 const range_2_element = document.getElementById("range-2");
@@ -51,28 +52,28 @@ const range_3_value_label = document.getElementById("range-3-value");
 const select_element = document.getElementById("select-1");
 const select_label = document.getElementById("select-value");
 
-range_1_element.addEventListener("input", function() {
+range_1_element.addEventListener("input", function () {
   const rng_1_value = Number(range_1_element.value); // range_1_element.value is a string by default, so we need to convert it to a number.
   range_1_value_label.innerHTML = `${rng_1_value}`; // Edit the text of the global var range_1_value
   g.rng_1_value = rng_1_value; // Assign the number to the global object.
   console.log(`g.rng_1_value is ${g.rng_1_value}`); // console.log is the easiest way to see a variable value in the javascript prompt.
 });
 
-range_2_element.addEventListener("input", function() {
+range_2_element.addEventListener("input", function () {
   const rng_2_value = Number(range_2_element.value);
   range_2_value_label.innerHTML = `${rng_2_value}`;
   g.rng_2_value = rng_2_value;
   console.log(`g.rng_2_value is ${g.rng_2_value}`);
 });
 
-range_3_element.addEventListener("input", function() {
+range_3_element.addEventListener("input", function () {
   const rng_3_value = Number(range_3_element.value);
   range_3_value_label.innerHTML = `${rng_3_value}`;
   g.rng_3_value = rng_3_value;
   console.log(`g.rng_3_value is ${g.rng_3_value}`);
 });
 
-select_element.addEventListener("change", function() {
+select_element.addEventListener("change", function () {
   const select_value = select_element.value;
   select_label.innerHTML = `Selection value is: <span style="color:orange" >${select_value}</span>.`
   g.select_value = select_value;
