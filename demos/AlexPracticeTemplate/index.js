2b2ce2414303d56f478a364ebf978d2f35b87669
Neil Hendren <neil.hendren@gmail.com>
2022-04-12 20:55:29 -0600

    merge conflict solved?

--- demos/AlexPracticeTemplate/index.js
+++ demos/AlexPracticeTemplate/index.js
@@ -5,6 +5,8 @@ window.g = {
   rng_2_value: 0,
   rng_3_value: 0,
   select_value: "value-1",
+  gate_angle: 45, // degrees
+  water_level: 10, // meters or ft
 }
 
 // See https://p5js.org/ to learn how to use this graphics library. setup() and draw() are used to draw on the canvas object of the page.  Seriously, spend some time learning p5.js because it will make drawing graphics a lot easier.  You can watch tutorial videos on the "Coding Train" youtube channel. They have a p5.js crash course under their playlists section.  It will make these functions make a lot more sense.
@@ -21,25 +23,58 @@ function setup() {
 
 // Whatever is included in draw() will be calculated at 60 fps.  It is basically a loop that calls itself every 16.67 ms. You can pause it at any time with the noLoop() function and start it again with the loop() function. Be sure to include every graphics statement in a push() / pop() statement, because it minimizes the chance that you accidentally apply styling or properties to another graphics object.
 function draw() {
-  background(250);
 
+  background(250);
   push();
 
   let step = frameCount % 600; // Modulus operator limits step variable to between 0 and 600.
-
   // An example of how to draw a graphic with the P5.js library.  This statement draws a line that moves across the canvas every 10 seconds (10000 ms). Recall that 16.67 ms * 600 = 10000 ms or 10 seconds.
-
-  stroke(255, 100, 200); // Color of the line across screen
+  fill(12); // come back and make brown
+  // stroke(100, 100, 200); // Color of the line across screen
   strokeWeight(3); // Width of the line
   // "line" syntax: line(x1, y2, x2, y2)
-  line(
-    (step / 600) * width,
-    height / 2 - 150,
-    (step / 600) * width,
-    height / 2 + 150
-  );
+
+
+  rect(100, 100, 15, 400)  // (x-cord, y-cord, x-length, y length)
+  rect(100, 500, 400, 15)  // bottom rectangle
+  pop()
+
+  push()
+
+
+  fill(0, 100, 200, 40)
+  strokeWeight(0); // Width of the line
+  translate(590, 710);                   // square portion of the water
+  rotate(PI)
+  rect(100, 200, 380, g.water_level * 30)
+
+  pop()
+
+  push()
+  fill(0, 100, 200, 40)                   // triangle poriton of the water
+  strokeWeight(0); // Width of the line
+  triangle(490, 500, 490, g.water_level * 21, 700, g.water_level * 21)
+
+  pop()
+
+  push();
+
+  translate(490, 510);
+  rotate(g.gate_angle * 2 * PI / 360 - Math.PI); // Gate Angle Settings
+  rect(-12.5, 0, 25, 400);
 
   pop();
+
+
+
+
+  push()
+  fill(128)
+  ellipse(490, 510, 40, 40);
+  pop();
+
+
+
 }
 
 // connects html to Javascript and assigns them to a const variable
@@ -53,10 +88,15 @@ const select_element = document.getElementById("select-1");
 const select_label = document.getElementById("select-value");
 
 range_1_element.addEventListener("input", function () {
+
   const rng_1_value = Number(range_1_element.value); // range_1_element.value is a string by default, so we need to convert it to a number.
+
   range_1_value_label.innerHTML = `${rng_1_value}`; // Edit the text of the global var range_1_value
   g.rng_1_value = rng_1_value; // Assign the number to the global object.
   console.log(`g.rng_1_value is ${g.rng_1_value}`); // console.log is the easiest way to see a variable value in the javascript prompt.
+  const angle = Number(range_1_element.value); // convert the value of the slider from a string to a number
+  g.gate_angle = angle;
+
 });
 
 range_2_element.addEventListener("input", function () {
@@ -64,6 +104,8 @@ range_2_element.addEventListener("input", function () {
   range_2_value_label.innerHTML = `${rng_2_value}`;
   g.rng_2_value = rng_2_value;
   console.log(`g.rng_2_value is ${g.rng_2_value}`);
+  const height = Number(range_2_element.value)
+  g.water_level = height;
 });
 
 range_3_element.addEventListener("input", function () {
