edfa07505d1b00afffbce63f40f38184132ca325
Neil Hendren <neil.hendren@gmail.com>
2022-04-15 13:35:08 -0600

    correct equation used for spring length

--- demos/IdealGasLaw/src/js/calcs.js
+++ demos/IdealGasLaw/src/js/calcs.js
@@ -27,14 +27,16 @@ function adiabatic_reversible() {
 function spring() {
   const block_height = 0.753; // the piston_height value corresponding to the blocks
   const extended_height = 0.5; // the piston_height value corresponding to the fully-extended springs
-  const k = 100000; // spring constant, N/m
+  const k = 60000; // spring constant, N/m
   const V_init = 0.03200403956884307; // initial volume, m^3
   let iterations = 0;
   function find_values() {
-    gvs.V = (gvs.P - P_atm) * gvs.A**2 / k + V_init;
+    // gvs.V = (gvs.P - P_atm) * gvs.A**2 / k + V_init;
+    gvs.spring_length = Math.min(0.1, Math.max(0, max_spring_length - (gvs.A / k) * (gvs.P - P_atm)));
+    gvs.V = V_init + (max_spring_length - gvs.spring_length) * gvs.A;
     gvs.P = gvs.n * gvs.R * gvs.T / gvs.V;
     gvs.T = (gvs.heat_added - P_atm * (gvs.V - V_init) - k * gvs.n * ( gvs.V**2 / 2 - V_init**2 / 2 - V_init * ( gvs.V - V_init ) ) / gvs.A**2 ) / ( gvs.Cv * gvs.n ) + T_init;
-    gvs.spring_length = 0.1 - (gvs.V - V_init) / gvs.A;
+    // gvs.spring_length = max_spring_length - (gvs.V - V_init) / gvs.A;
     gvs.piston_height = block_height - (gvs.spring_length / max_spring_length) * (block_height - extended_height);
     if(iterations < 100) {
       iterations++;
