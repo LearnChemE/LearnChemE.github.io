157ccb8cc0ed41e4177d9d6cfedcce87062ee33b
Neil Hendren <neil.hendren@gmail.com>
2022-10-27 16:36:56 -0500

    solve

---
+++ demos/Parallel and Counterflow Heat Exchangers/PracticeTemplate/solve.js
@@ -0,0 +1,12 @@
+function solve(args) {
+    hot_temperatures = [400, 400, 400, 400];
+    cold_temperatures = [300, 300, 300, 300];
+
+    for(let rounds = 0; rounds < 100; rounds++) {
+        for(let i = 0; i < hot_temperatures.length; i++) {
+            // calculate heat transfer
+            // use eulers to find temperature of next point
+            // repeat
+        }
+    }
+}
