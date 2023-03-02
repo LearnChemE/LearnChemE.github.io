03b91d67f93e2ffe1e61328cef5ddeb241ef0e0c
Neil Hendren <neil.hendren@gmail.com>
2023-03-02 13:11:07 -0700

    Rankine Cycle initial commit

---
+++ demos/RankineCycle/src/index.js
@@ -0,0 +1,38 @@
+require("bootstrap");
+require("./style/style.scss");
+window.p5 = new require("./js/p5.min.js");
+
+// TO DO:
+
+// GLOBAL VARIABLES OBJECT
+window.gvs = {
+
+};
+
+const containerElement = document.getElementById("p5-container");
+
+const sketch = (p) => {
+
+    p.setup = function() {
+        p.createCanvas(800, 530);
+        p.noLoop();
+        gvs.p = p;
+        gvs.drawAll = require("./js/draw.js");
+        require("./js/initialize.js");
+        gvs.calcAll = require("./js/calcs.js");
+        require("./js/inputs.js");
+        
+        p.windowResized = function() {
+
+        }
+    };
+
+    p.draw = function() {
+        p.background(253);
+        gvs.calcAll();
+        gvs.drawAll(p);
+    };
+
+};
+
+const P5 = new p5(sketch, containerElement);
