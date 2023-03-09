e94f006b5272b405e35202780bd0c43cc79956ee
Patrick <pado7068@colorado.edu>
2023-03-09 13:54:50 -0700

    changed to moving the dot around with mouse

--- demos/LeverRuleForBinaryMixtureOnPXYdiagram/functions.js
+++ demos/LeverRuleForBinaryMixtureOnPXYdiagram/functions.js
@@ -3,7 +3,7 @@
 function graphDraw(){
     push();
     rect(75,50,400,390);
-    rect(g.rx,g.ty,175,390)
+    rect(g.rx,g.ty,200,390)
 
     let xLabels = ['0.0','0.2','0.4','0.6','0.8','1.0'];
     let yLabels = ['3','4','5','6','7','8','9','10'];
@@ -56,25 +56,12 @@ function graphDraw(){
     // X & Y-axis labels
     push();
     textSize(18); noStroke();
-    text('Mole Fraction A',g.lx+130,g.by+45);
+    text('Mole fraction benzene',g.lx+105,g.by+45);
     translate(40,height/2+40);
     rotate(radians(-90));
     text('Pressure (kPa)',0,0);
     pop();
 
-    // Right panel labels
-    textSize(15); noStroke();
-    text('Relative amounts',g.rx+35,g.ty+20);
-    textSize(14); textStyle(ITALIC);
-    text('x  = ',g.rx+25,g.by-20);
-    text('y  = ',g.rx+105,g.by-20);
-    textStyle(NORMAL); 
-    text(g.xbz,g.rx+50,g.by-20);
-    text(g.ybz,g.rx+130,g.by-20);
-    textSize(11);
-    text('A',g.rx+32,g.by-15);
-    text('A',g.rx+112,g.by-15);
-    pop();
 
     // Liquid curve
     let y1 = g.pStarB; let y2 = g.pStarA;
@@ -106,17 +93,23 @@ function graphDraw(){
     }
     endShape();
     pop();
+
+    
 }
 
 function everythingElse(){
 
+    let temp = g.points[0];
+    ellipse(temp.x,temp.y,5)
     let x,y;
 
     // Location of purple dot
     push();
-    x = map(g.moleFrac,0,1,g.lx,g.rx);
-    y = map(g.pressure,2.4,10,g.by,g.ty);
+    x = temp.x
+    y = temp.y
     
+    mole = map(x,g.lx,g.rx,0,1);
+    pres = map(temp.y,g.by,g.ty,2.4,10);
     
 
     // Determining if the dot is within the two curves
@@ -132,29 +125,28 @@ function everythingElse(){
     }
 
     // Pixel value of y-coord of vapor curve at the x-coord of purple dot
-    let ytest2 = map((g.pStarA*g.pStarB)/(g.pStarA + (g.pStarB-g.pStarA)*g.moleFrac),2.4,10,g.by,g.ty);
+    let ytest2 = map((g.pStarA*g.pStarB)/(g.pStarA + (g.pStarB-g.pStarA)*mole),2.4,10,g.by,g.ty);
     if(y < ytest2){ // Purple dot y-coord above vapor curve
         test2 = true;
     } else {
         test2 = false;
     }
-
-    console.log(test1,test2)
     
     let xR;
     let xRpx, xLpx;
     if(test1 && test2){
-        xR = ((g.pStarA*g.pStarB)/g.pressure - g.pStarA)/(g.pStarB - g.pStarA);
+        xR = ((g.pStarA*g.pStarB)/pres - g.pStarA)/(g.pStarB - g.pStarA);
         xRpx = map(xR,0,1,g.lx,g.rx);
         xLpx = (y - g.liquidLine[1])/(g.liquidLine[0]);
 
         push();
         drawingContext.setLineDash([5,5]); strokeWeight(1.5);
-        stroke(g.blue);
-        line(x,y,xRpx,y); // Purple dot to vapor curve
         stroke(g.red);
+        line(x,y,xRpx,y); // Purple dot to vapor curve
+        stroke(g.blue);
         line(x,y,xLpx,y); // Purple dot to liquid curve
         drawingContext.setLineDash([1,6]);
+        stroke(g.red);
         line(xLpx,y,xLpx,g.by); // Liquid curve intersection down to mole frac axis
         stroke(g.blue);
         line(xRpx,y,xRpx,g.by); // Vapor curve intersection down to mole frac axis
@@ -182,37 +174,93 @@ function everythingElse(){
         
         push();
         strokeWeight(.5); fill(g.red);
-        rect(g.rx+35,g.by-40,30,height1);
+        rect(g.rx+65,g.by-40,30,height1);
         fill(g.blue);
-        rect(g.rx+115,g.by-40,30,height2);
+        rect(g.rx+130,g.by-40,30,height2);
+        pop();
+
+        // Right panel labels
+        textSize(15); noStroke();
+        text('Liquid and vapor',g.rx+50,g.ty+20);
+        text('relative amounts',g.rx+50,g.ty+35);
+        textSize(14); textStyle(ITALIC);
+        text('x  = ',g.rx+55,g.by-20);
+        text('y  = ',g.rx+120,g.by-20);
+        textStyle(NORMAL); 
+        text(g.xbz,g.rx+80,g.by-20);
+        text(g.ybz,g.rx+145,g.by-20);
+        textSize(11);
+        text('B',g.rx+62,g.by-15);
+        text('B',g.rx+127,g.by-15);
         pop();
 
     } else if (test1 && !test2){
         push();
         strokeWeight(.5); fill(g.blue);
-        rect(g.rx+115,g.by-40,30,-300);
+        rect(g.rx+130,g.by-40,30,-300);
         pop();
         g.ybz = (map(x,g.lx,g.rx,0,1)).toFixed(2);
         g.xbz = (0).toFixed(2);
+
+        // Right panel labels
+        textSize(15); noStroke();
+        text('Liquid and vapor',g.rx+50,g.ty+20);
+        text('relative amounts',g.rx+50,g.ty+35);
+        textSize(14); textStyle(ITALIC);
+        text('y  = ',g.rx+120,g.by-20);
+        textStyle(NORMAL); 
+        text(g.ybz,g.rx+145,g.by-20);
+        textSize(11);
+        text('B',g.rx+127,g.by-15);
+        pop();
     } else if (!test1 && test2){
         push();
         strokeWeight(.5); fill(g.red);
-        rect(g.rx+35,g.by-40,30,-300);
+        rect(g.rx+65,g.by-40,30,-300);
         pop();
         g.ybz = (0).toFixed(2);
         g.xbz = (map(x,g.lx,g.rx,0,1)).toFixed(2);
+        // Right panel labels
+        textSize(15); noStroke();
+        text('Liquid and vapor',g.rx+50,g.ty+20);
+        text('relative amounts',g.rx+50,g.ty+35);
+        textSize(14); textStyle(ITALIC);
+        text('x  = ',g.rx+55,g.by-20);
+        textStyle(NORMAL); 
+        text(g.xbz,g.rx+80,g.by-20);
+        textSize(11);
+        text('B',g.rx+62,g.by-15);
+        pop();
     }
 
-    push();
-    drawingContext.setLineDash([5,5]);
-    strokeWeight(.5);
-    line(g.rx+20,g.by-340,g.rx+160,g.by-340);
-    noStroke(); textSize(12);
-    text('Maximum',g.rx+60,g.by-345);
-    pop();
 
+    // Right panel labels
+    //rect(g.rx+50,g.by-40,30,-300);
+    let count = 0; let xLabels = ['0.0','0.2','0.4','0.6','0.8','1.0'];
+    push(); strokeWeight(.5); textSize(15);
+    for(let i = 0; i < 20; i++){
+        if(i%4 == 0){
+            line(g.rx+65,g.by-40-300*i/20,g.rx+75,g.by-40-300*i/20);
+            line(g.rx+160,g.by-40-300*i/20,g.rx+150,g.by-40-300*i/20);
+            text(xLabels[count],g.rx+40,g.by-35-300*i/20);
+            count++;
+
+        } else {
+            line(g.rx+65,g.by-40-300*i/20,g.rx+70,g.by-40-300*i/20);
+            line(g.rx+160,g.by-40-300*i/20,g.rx+155,g.by-40-300*i/20);
+        }
+        
+    }
+    line(g.rx+80,g.by-40,g.rx+80,g.by-45);
+    line(g.rx+145,g.by-40,g.rx+145,g.by-45);
+    text(xLabels[5],g.rx+40,g.by-335);
+    noFill();
+    
+    rect(g.rx+65,g.by-40,95,-300);
+    pop();
 
     noStroke(); fill(150,0,200);
-    ellipse(x,y,10);
+    ellipse(temp.x,temp.y,10);
     pop();
+    
 }
--- demos/LeverRuleForBinaryMixtureOnPXYdiagram/index.js
+++ demos/LeverRuleForBinaryMixtureOnPXYdiagram/index.js
@@ -2,9 +2,6 @@
 window.g = {
     cnv: undefined,
 
-    moleFrac: 0.5,
-    pressure: 5,
-
     xbz: 0.5,
     ybz: 0.5,
 
@@ -20,8 +17,14 @@ window.g = {
     ty: 50,
 
     // Colors to be used repeatedly
-    red: [100,0,0],
+    red: [175,0,0],
     blue: [0,70,250],
+
+    // For moving the dot around
+    radius: 8,
+    points: [],
+    nP: 1,
+    dragPoint: null,
 
 }
 
@@ -29,6 +32,11 @@ function setup(){
     g.cnv = createCanvas(700,500);
     g.cnv.parent("graphics-wrapper");
     document.getElementsByTagName("main")[0].remove();
+
+    let x, y;
+    x = map(.5,0,1,g.lx,g.rx);
+    y = map(5,2.4,10,g.by,g.ty);
+    g.points.push(createVector(x,y));
 
 }
 
@@ -39,22 +47,55 @@ function draw() {
     
 }
 
+// For manipulating the position of the purple dot
+function mousePressed() {
+    for (let i = g.points.length - 1; i >= 0; i--) {
+      const isPressed = inCircle(g.points[i], g.radius);
+      if (isPressed) {
+        g.dragPoint = g.points.splice(i, 1)[0];
+        g.points.push(g.dragPoint);
+      }
+    }
+}
 
+function mouseDragged() {
 
-const moleFrac = document.getElementById("mole-fraction");
-const moleFracLabel = document.getElementById("mole-fraction-value");
+    if(g.dragPoint){
+        if(mouseX >= g.lx && mouseX <= g.rx && mouseY >= g.ty && mouseY <= g.by){ // Within the graph
+            g.dragPoint.x = mouseX;
+            g.dragPoint.y = mouseY;
+        } else if (mouseX < g.lx && mouseY >= g.ty && mouseY <= g.by){ // Left of the graph
+            g.dragPoint.x = g.lx;
+            g.dragPoint.y = mouseY;
+        } else if (mouseX < g.lx && mouseY < g.ty){ // Upper left corner of the graph
+            g.dragPoint.x = g.lx;
+            g.dragPoint.y = g.ty;
+        } else if (mouseX >= g.lx && mouseX <= g.rx && mouseY < g.ty){ // Above the graph
+            g.dragPoint.x = mouseX;
+            g.dragPoint.y = g.ty;
+        } else if (mouseX > g.rx && mouseY < g.ty){ // Upper right corner of the graph
+            g.dragPoint.x = g.rx;
+            g.dragPoint.y = g.ty;
+        } else if (mouseX > g.rx && mouseY >= g.ty && mouseY <= g.ty){ // Right of the graph
+            g.dragPoint.x = g.rx;
+            g.dragPoint.y = mouseY;
+        } else if (mouseX > g.rx && mouseY > g.by){ // Lower right corner of the graph
+            g.dragPoint.x = g.rx;
+            g.dragPoint.y = g.by;
+        } else if (mouseX <= g.rx && mouseX >= g.lx && mouseY > g.by){ // Below the graph
+            g.dragPoint.x = mouseX;
+            g.dragPoint.y = g.by;
+        } else if (mouseX < g.lx && mouseY > g.by){
+            g.dragPoint.x = g.lx;
+            g.dragPoint.y = g.by;
+        }
+    }
+}
 
-const pressure = document.getElementById("pressure");
-const pressureLabel = document.getElementById("pressure-value");
+function mouseReleased() {
+    g.dragPoint = null;
+}
 
-moleFrac.addEventListener("input", function (){
-    const temp = Number(moleFrac.value);
-    moleFracLabel.innerHTML = `${temp}`;
-    g.moleFrac = temp;
-});
-
-pressure.addEventListener("input", function (){
-    const temp = Number(pressure.value);
-    pressureLabel.innerHTML = `${temp}`;
-    g.pressure = temp; 
-});
+function inCircle(pos, radius) {
+    return dist(mouseX, mouseY, pos.x, pos.y) < radius;
+}
