6e22b4ba57d0d6d341b1df1ddc0186194b8e9052
Abijith Trichur Ramachandran <trabijith@gmail.com>
2023-11-30 21:06:17 -0700

    Fixed minor bugs

--- demos/ChemicalEquilibriumInHaberProcess/index.js
+++ demos/ChemicalEquilibriumInHaberProcess/index.js
@@ -74,7 +74,7 @@ const config = {
     scales: {
       y: {
         beginAtZero: true,
-        max: 12,
+        max: 21,
         ticks: {
           font: function (context) {
             var width = context.chart.width;
@@ -179,19 +179,73 @@ function initValues() {
 
 
 //  ----------------------------------------------------------------
+// function calculateEquilibrium(P, T, nN2, nH2, nNH3) {
+//   P = Number(P);
+//   T = Number(T);
+//   nN2 = Number(nN2);
+//   nH2 = Number(nH2);
+//   nNH3 = Number(nNH3);
+
+//   const K_T_value = -23.84 + (11051 / T);
+//   const K_T = parseFloat(Math.exp(K_T_value).toFixed(3));
+//   const RHS = Math.pow(P, 2) * K_T;
+
+//   let xi_lower = 0;
+//   xi_upper = Math.min(xi_upper, nN2, nH2 / 3);
+
+//   let xi = (xi_lower + xi_upper) / 2;
+//   const tolerance = 0.001;
+//   const maxIterations = 10000;
+
+//   for (let i = 0; i < maxIterations; i++) {
+//     let value = (25 * Math.pow((2 * xi + nNH3), 2)) / ((nN2 - xi) * (nH2 - 3 * xi));
+
+//     if (Math.abs(value - RHS) < tolerance) {
+//       break;
+//     }
+
+//     if (value < RHS) {
+//       xi_lower = xi;
+//     } else {
+//       xi_upper = xi;
+//     }
+//     xi = (xi_lower + xi_upper) / 2;        
+//   }
+
+//   if (xi_upper - xi_lower > tolerance) {
+//     console.log("Equilibrium not found within tolerance.");
+//     return null;
+//   }
+
+//   let nN2Final = nN2 - xi;
+//   let nH2Final = nH2 - (3 * xi);
+//   let nNH3Final = Number(nNH3) + (2 * xi);
+//   return [
+//     parseFloat(nN2Final.toFixed(3)),
+//     parseFloat(nH2Final.toFixed(3)),
+//     parseFloat(nNH3Final.toFixed(3))
+//   ];
+// }
 
 function calculateEquilibrium(P, T, nN2, nH2, nNH3) {
+  P = Number(P);
+  T = Number(T);
+  nN2 = Number(nN2);
+  nH2 = Number(nH2);
+  nNH3 = Number(nNH3);
+
   const K_T_value = -23.84 + (11051 / T);
-  const K_T = Math.exp(K_T_value);
+  const K_T = parseFloat(Math.exp(K_T_value).toFixed(3));
   const RHS = Math.pow(P, 2) * K_T;
 
   let xi_lower = 0;
-  let xi_upper = Math.min(nN2, nH2 / 3);  // upper bound for xi
+  let xi_upper = Math.min(nN2 + (2 * nNH3), (nH2 + (3 * nNH3)) / 3);
+  xi_upper = Math.min(xi_upper, nN2, nH2 / 3);
+
   let xi = (xi_lower + xi_upper) / 2;
-  const tolerance = 0.01;
+  const tolerance = 0.000001;
   const maxIterations = 10000;
 
-  //Identifying the value of xi that fits within the equation
   for (let i = 0; i < maxIterations; i++) {
     let value = (25 * Math.pow((2 * xi + nNH3), 2)) / ((nN2 - xi) * (nH2 - 3 * xi));
 
@@ -205,7 +259,6 @@ function calculateEquilibrium(P, T, nN2, nH2, nNH3) {
       xi_upper = xi;
     }
     xi = (xi_lower + xi_upper) / 2;
-
   }
 
   if (xi_upper - xi_lower > tolerance) {
@@ -216,7 +269,11 @@ function calculateEquilibrium(P, T, nN2, nH2, nNH3) {
   let nN2Final = nN2 - xi;
   let nH2Final = nH2 - (3 * xi);
   let nNH3Final = Number(nNH3) + (2 * xi);
-  return [parseFloat(nN2Final.toFixed(3)), parseFloat(nH2Final.toFixed(3)), parseFloat(nNH3Final.toFixed(3))];
+  return [
+    parseFloat(nN2Final.toFixed(3)),
+    parseFloat(nH2Final.toFixed(3)),
+    parseFloat(nNH3Final.toFixed(3))
+  ];
 }
 
 
