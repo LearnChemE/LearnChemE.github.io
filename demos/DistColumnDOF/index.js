b8c4fb23f7d689a3144a7afdc83422be343989c2
drew <drsm1370@colorado.edu>
2024-07-08 14:14:54 -0600

    fixed text and added zb display

--- demos/DistColumnDOF/index.js
+++ demos/DistColumnDOF/index.js
@@ -3,31 +3,32 @@ var dropdownvalue = "condenser";
 window.g = {
     unknowns: 1,
     cnv: undefined,
-    opt : 1, 
+    opt: 1,
+    showZb: false,
 };
 
 //storing variables
 let textElements = [
-    { text: 'm5', x: 690, y: 500 },
-    { text: 'm1', x: 60, y: 290 },
-    { text: 'y7,A \n 1-y7,A', x: 320, y: 410 },
-    { text: 'x5,A \n 1-x5,A', x: 684, y: 530 },
-    { text: 'm3', x: 330, y: 500 },
-    { text: 'y4,A \n 1-y4,A', x: 684, y: 130 },
-    { text: 'z1,A \n 1-z1,A', x: 50, y: 320 },
-    { text: 'm4', x: 684, y: 100 },
-    { text: 'm2', x: 330, y: 100 },
-    { text: 'm6', x: 330, y: 220 },
-    { text: 'm7', x: 330, y: 380 },
-    { text: 'x6,A \n 1-x6,A', x: 320, y: 250 },
-    { text: 'y2,A \n 1-y2,A', x: 320, y: 130 },
-    { text: 'x3,A \n 1-x3,A', x: 320, y: 530 },
+    { mainText: 'm', sub: '5', x: 694, y: 500, size: 20, xsub: 8 },
+    { mainText: 'm', sub: '1', x: 60, y: 290, size: 20, xsub: 8 },
+    { mainText: '   y \n 1-y ', sub: '7,A \n \n 7,A', x: 320, y: 410, size: 20, xsub: 5, altText: '   y \n   y', altSub: '7,A \n \n7,B' },
+    { mainText: '   x \n 1-x ', sub: '5,A \n \n 5,A', x: 684, y: 530, size: 20, xsub: 5, altText: '   x \n   x', altSub: '5,A \n \n5,B' },
+    { mainText: 'm', sub: '3', x: 330, y: 500, size: 20, xsub: 8 },
+    { mainText: '   y \n 1-y ', sub: '4,A \n \n 4,A', x: 684, y: 130, size: 20, xsub: 5, altText: '   y \n   y', altSub: '4,A \n \n4,B' },
+    { mainText: '   z \n 1-z ', sub: '1,A \n \n  1,A', x: 50, y: 320, size: 20, xsub: 5, altText: '   z \n   z', altSub: '1,A \n \n1,B' },
+    { mainText: 'm', sub: '4', x: 694, y: 100, size: 20, xsub: 8 },
+    { mainText: 'm', sub: '2', x: 330, y: 100, size: 20, xsub: 8 },
+    { mainText: 'm', sub: '6', x: 330, y: 220, size: 20, xsub: 8 },
+    { mainText: 'm', sub: '7', x: 330, y: 380, size: 20, xsub: 8 },
+    { mainText: '   x \n 1-x ', sub: '6,A \n \n 6,A', x: 320, y: 250, size: 20, xsub: 5, altText: '   x \n   x', altSub: '6,A \n \n6,B' },
+    { mainText: '   y \n 1-y ', sub: '2,A \n \n 2,A', x: 320, y: 130, size: 20, xsub: 5, altText: '   y \n   y', altSub: '2,A \n \n2,B' },
+    { mainText: '   x \n 1-x ', sub: '3,A \n \n 3,A', x: 320, y: 530, size: 20, xsub: 5, altText: '   x \n   x', altSub: '3,A \n \n3,B' },
 ];
 
 function shuffleArray(array) {
     console.log('shuffling');
-    for(let i=array.length - 1;i>0;i--) {
-        var j=Math.floor(Math.random() * (i+1));
+    for (let i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
 
         var temp = array[i];
         array[i] = array[j];
@@ -37,8 +38,16 @@ function shuffleArray(array) {
     return array;
 }
 
-$("#shuffle-btn").click(() => {
-    shuffleArray(textElements);
+$(() => {
+    $("#shuffle-btn").click(() => {
+        shuffleArray(textElements);
+    });
+    $("#equationB").click(() => {
+        g.showZb = false;
+    });
+    $("#fractionB").click(() => {
+        g.showZb = true;
+    });
 });
 
 
@@ -49,33 +58,31 @@ const unknownsLabel = document.getElementById("unknown-label");
 const defaultColor = 'black';
 const activeColor = 'blue';
 
-unknowns.addEventListener("input", function() {
+unknowns.addEventListener("input", function () {
     let unk = Number(unknowns.value);
     g.unknowns = unk;
     unknownsLabel.innerHTML = `${unk}`;
     // drawVar(unk);
 });
 
-import { drawRectangle, drawArrow, drawBorder, drawText} from './functions.mjs';
+import { drawRectangle, drawArrow, drawBorder, drawText, drawSub } from './functions.mjs';
 
-function drawTextElements() {
-    push();
-    textElements.forEach(element => {
-        fill(defaultColor); noStroke();
-        text(element.text, element.x, element.y);
-    });
-    pop();
-}
+function drawTextElements(nUnknowns) {
+    let element;
 
-function drawActiveTextElements(nUnknowns) {
     push();
-    textElements.slice(0, nUnknowns).forEach(element => {
+    for (let i = 0; i < nUnknowns; i++) {
+        element = textElements[i];
         fill(activeColor); //stroke(activeColor); //noStroke();
-        text(element.text, element.x, element.y);
-    });
+        drawSub(element.mainText, element.sub, element.x, element.y, element.size, element.xsub, element.altText, element.altSub, g.showZb);
+    }
+    for (let i = nUnknowns; i < textElements.length; i++) {
+        element = textElements[i];
+        fill(defaultColor); //stroke(activeColor); //noStroke();
+        drawSub(element.mainText, element.sub, element.x, element.y, element.size, element.xsub, element.altText, element.altSub, g.showZb);
+    }
     pop();
 }
-
 
 function drawDisplay(value) {
     // console.log(value);
@@ -85,13 +92,13 @@ function drawDisplay(value) {
     let contot = 'black';
     let rebtot = 'black';
     let coltot = 'black';
-    
+
     switch (value) {
         case 'reboiler':
             borderVars = [475, 420, 200, 180, 10, 'green'];
             colreb = 'green';
             rebtot = 'green';
-          
+
             break;
         case 'condenser':
             borderVars = [475, 20, 200, 180, 10, 'green'];
@@ -104,16 +111,16 @@ function drawDisplay(value) {
             colcon = 'green';
             colreb = 'green';
             coltot = 'green';
-            
+
             break;
         case 'overall':
             borderVars = [60, 30, 670, 560, 10, 'green'];
             contot = 'green';
             rebtot = 'green';
             coltot = 'green';
-            
+
             break;
-            
+
     }
 
     push();
@@ -124,7 +131,7 @@ function drawDisplay(value) {
 
     // Green dotted border
     drawBorder(...borderVars);
-    
+
     // col/condenser
     drawArrow([200, 110], [500, 110], 10, colcon);
     drawArrow([575, 230], [270, 230], 10, colcon);
@@ -153,29 +160,30 @@ function drawDisplay(value) {
     line(575, 170, 575, 230);
 
     drawText(value, 940, 120, 'black', true);
-   
-    drawTextElements();
+
+    // drawTextElements();
     pop();
 }
 
 function drawVar(nUnknowns) {
     clear();
     drawDisplay(dropdownvalue); // Ensure the display is updated
-    drawActiveTextElements(nUnknowns); // Draw the active text elements 
+    textSize(18);
+    drawTextElements(nUnknowns);
 }
 
 function setup() {
     // console.log('state 0');
     g.cnv = createCanvas(1100, 600);
-    g.cnv.parent("graphics-wrapper");    
+    g.cnv.parent("graphics-wrapper");
 }
 
-document.getElementById('unknown').addEventListener('change', function() {
-    g.unknowns = Number(this.value); 
+document.getElementById('unknown').addEventListener('change', function () {
+    g.unknowns = Number(this.value);
     // drawVar(g.unknowns);
 });
 
-document.getElementById('isotype').addEventListener('change', function() {
+document.getElementById('isotype').addEventListener('change', function () {
     dropdownvalue = this.value;
     // drawDisplay(dropdownvalue);
 });
@@ -185,7 +193,7 @@ function draw() {
     background(255);
     drawVar(g.unknowns);
     // updateView();
-    
+
 }
 
 // document.addEventListener("DOMContentLoaded", function() {
@@ -193,20 +201,20 @@ function draw() {
 //     //  references to radio buttons
 //     const equationB = document.getElementById('equationB');
 //     const fractionB = document.getElementById('fractionB');
-    
-    
+
+
 //     // Initial update (in case the initial state needs to be set)
 //     // updateView();
 // });
 
 // Event listeners for radio buttons
-equationB.addEventListener('change', function() {
+equationB.addEventListener('change', function () {
     if (this.checked) {
         g.opt = 1; // Set to the option for '1-zA'
         // updateView();
     }
 });
-fractionB.addEventListener('change', function() {
+fractionB.addEventListener('change', function () {
     if (this.checked) {
         g.opt = 2; // Set to the option for 'zB'
         // updateView();
@@ -224,7 +232,7 @@ function updateView() {
 
 function updateForEquationB() {
     console.log("Updating display for Equation B (1-zA)");
-    drawDisplay('condenser'); 
+    drawDisplay('condenser');
     if (window.myP5) {
         window.myP5.drawEquationB();
     }
