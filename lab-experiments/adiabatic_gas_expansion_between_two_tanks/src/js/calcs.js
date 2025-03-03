1d44f704192fbab15ffc7aa7220283ce4cabc648
NeilTheSeal <neil.hendren@gmail.com>
2025-03-03 16:57:53 -0700

    Add tanks and pressure release valve and related logic

--- lab-experiments/adiabatic_gas_expansion_between_two_tanks/src/js/calcs.js
+++ lab-experiments/adiabatic_gas_expansion_between_two_tanks/src/js/calcs.js
@@ -1,9 +1,101 @@
 export function setDefaults() {
   state = {
-    ...state
+    ...state,
+    leftTank: {
+      volume: 100,
+      pressure: 1e7,
+      outletPressure: 0,
+      temperature: 2,
+      valveRotation: 0,
+      xCoord: 30,
+      yCoord: 30,
+      width: 20,
+      height: 80,
+    },
+    rightTank: {
+      volume: 100,
+      pressure: 0,
+      outletPressure: 0,
+      temperature: 0,
+      valveRotation: 0,
+      xCoord: 100,
+      yCoord: 30,
+      width: 20,
+      height: 80,
+    },
+    valvePosition: 0,
+    solution: {
+      Tf1: 0,
+      Tf2: 0,
+      P: 0,
+    }
+  };
+
+  state.valveLocation = {
+    x: width / 2 / relativeSize(),
+    y: state.leftTank.yCoord - 10,
+    width: 5,
+    height: 1,
+  }
+}
+
+export function solve() {
+  const V1 = state.leftTank.volume;
+  const V2 = state.rightTank.volume;
+  const R = 8.314;
+  const Pi = state.leftTank.pressure / 1000;
+  const Ti = state.leftTank.temperature + 273.15;
+  let Tf1, Tf2, Pff;
+  let error = 1e12;
+  for (let T1 = Ti; T1 > 0; T1 -= 1) {
+    for (let T2 = Ti; T2 < 1000; T2 += 1) {
+      for (let Pf = 0; Pf < state.leftTank.pressure / 1000; Pf += 100) {
+        const diff1 = Pi * V1 / Ti - Pf * V1 / T1 - Pf * V2 / T2;
+        const diff2 = T1 / Ti - (Pf / Pi) ** (2 / 7);
+        const diff3 = Pi * V1 - Pf * V1 - Pf * V2;
+        const diff = abs(diff1) + abs(diff2) + abs(diff3);
+        if (diff < error) {
+          error = diff;
+          Tf1 = T1;
+          Tf2 = T2;
+          Pff = Pf;
+        }
+      }
+    }
+  }
+  for (let T1 = Tf1 + 20; T1 > Tf1 - 20; T1 -= 1) {
+    for (let T2 = Tf2 + 20; T2 > Tf2 - 20; T2 -= 1) {
+      for (let Pf = Pff - 50; Pf < Pff + 50; Pf++) {
+        const diff1 = Pi * V1 / Ti - Pf * V1 / T1 - Pf * V2 / T2;
+        const diff2 = T1 / Ti - (Pf / Pi) ** (2 / 7);
+        const diff3 = Pi * V1 - Pf * V1 - Pf * V2;
+        const diff = abs(diff1) + abs(diff2) + abs(diff3);
+        if (diff < error) {
+          error = diff;
+          Tf1 = T1;
+          Tf2 = T2;
+          Pff = Pf;
+        }
+      }
+    }
+  }
+  state.solution = {
+    Tf1: Tf1,
+    Tf2: Tf2,
+    P: Pff * 1000,
   }
 }
 
 export function calcAll() {
-
+  if (state.rightTank.valveRotation > 0 && state.leftTank.valveRotation > 0 && state.valvePosition > 0) {
+    const Tf1 = state.solution.Tf1;
+    const Tf2 = state.solution.Tf2;
+    const Pf = state.solution.P;
+    state.leftTank.temperature = 0.005 * (state.leftTank.temperature - Tf1) + state.leftTank.temperature;
+    state.rightTank.temperature = 0.005 * (state.rightTank.temperature - Tf2) + state.rightTank.temperature;
+    state.leftTank.pressure = 0.005 * (Pf - state.leftTank.pressure) + state.leftTank.pressure;
+    state.rightTank.pressure = 0.005 * (Pf - state.rightTank.pressure) + state.rightTank.pressure;
+    state.leftTank.outletPressure = 0.05 * (state.leftTank.pressure - state.leftTank.outletPressure) + state.leftTank.outletPressure;
+    state.rightTank.outletPressure = 0.05 * (state.leftTank.outletPressure - state.rightTank.outletPressure) + state.rightTank.outletPressure;
+  }
 }
--- lab-experiments/adiabatic_gas_expansion_between_two_tanks/src/js/draw.js
+++ lab-experiments/adiabatic_gas_expansion_between_two_tanks/src/js/draw.js
@@ -1,3 +1,230 @@
-export function drawAll() {
+function drawLeftTank() {
+  const x = state.leftTank.xCoord;
+  const y = state.leftTank.yCoord;
+  const width = state.leftTank.width;
+  const height = state.leftTank.height;
+  drawTank(x, y, width, height, state.leftTank);
+}
 
+function drawRightTank() {
+  const x = state.rightTank.xCoord;
+  const y = state.rightTank.yCoord;
+  const width = state.rightTank.width;
+  const height = state.rightTank.height;
+  drawTank(x, y, width, height, state.rightTank);
+}
+
+function drawTank(x, y, width, height, tank) {
+  push();
+  drawRegulator(x + width / 2, y, 5, tank);
+  const tankColor = "rgb(190, 190, 190)";
+  const tankOutline = "rgb(100, 100, 100)";
+  fill(tankColor);
+  stroke(tankOutline);
+  strokeWeight(0.1);
+  rect(x, y, width, height, width / 2, width / 2, width / 10, width / 10);
+  pop();
+}
+
+function drawRegulator(x, y, size, tank) {
+  push();
+  translate(x, y);
+  const knobColor = "rgb(215, 215, 215)";
+  const knobOutline = "rgb(180, 180, 180)";
+  const metalColor = "rgb(230, 230, 230)";
+  const metalOutline = "gray";
+  push();
+  fill(metalColor);
+  stroke(metalOutline);
+  strokeWeight(0.1);
+  translate(0, -size);
+  rectMode(CENTER);
+  if (tank === state.rightTank) {
+    rect(-size * 1.1, 0, size * 2, size / 3, size / 10);
+    fill(240);
+    rect(-size * 1.5, -size / 3, size / 8, size / 3);
+    rect(-size / 3.5, 0, size / 6, size / 2, size / 20);
+    rect(size / 1.2, 0, size, size / 8);
+    rect(size * 1.4, -size / 4, size / 16, size / 2, size / 40);
+    rect(size * 1.4, 0, size / 6, size / 6, size / 40);
+    rect(size / 3.5, 0, size / 6, size / 2, size / 20);
+  } else {
+    rect(size * 1.1, 0, size * 2, size / 3, size / 10);
+    fill(240);
+    rect(size * 1.5, -size / 3, size / 8, size / 3);
+    rect(-size / 1.2, 0, size, size / 8);
+    rect(-size * 1.4, -size / 4, size / 16, size / 2, size / 40);
+    rect(-size * 1.4, 0, size / 6, size / 6, size / 40);
+    rect(-size / 3.5, 0, size / 6, size / 2, size / 20);
+    rect(size / 3.5, 0, size / 6, size / 2, size / 20);
+  }
+  pop();
+  fill(knobColor);
+  stroke(knobOutline);
+  strokeWeight(0.1);
+  const time = tank.valveRotation;
+  const yPos = -3 * size / 2 - 1.05 * size / 4;
+  const knobHeight = 1.1 * size / 4;
+  for (let i = 0; i < 6; i++) {
+    const offsetTime = (time + 0.16667 * i) % 1;
+    const xPos = 1 * cos(offsetTime * TWO_PI) * size / 2;
+    const w = (size / 10) * abs(sin((0.25 - (offsetTime - 0.25) % 0.25) * TWO_PI)) + size / 20;
+    if (offsetTime > 0 && offsetTime < 0.5) {
+      rect(xPos - w / 2, yPos, size / 3 * w, knobHeight, size / 40);
+    }
+  }
+  fill(metalColor);
+  stroke(metalOutline);
+  rect(-size / 2, -size / 2, size, size, size / 10, size / 10, size / 10, size / 10);
+  rect(-size / 4, -size / 2, size / 2, -size, size / 20);
+  tank.knobCoords = [x - size / 2, y - 31 * size / 16, size, size / 2];
+  rect(-size / 2, -3 * size / 2, size, -size / 4, size / 20);
+  fill(knobColor);
+  stroke(knobOutline);
+  for (let i = 0; i < 3; i++) {
+    const offsetTime = (time + 0.16667 * i + 0.5) % 0.5;
+    const xPos = -1 * cos(offsetTime * TWO_PI) * size / 2;
+    const w = (size / 10) * abs(sin((0.25 - (offsetTime - 0.25) % 0.25) * TWO_PI)) + size / 20;
+    if (offsetTime < 0.5 && offsetTime > 0) {
+      rect(xPos - w / 2, yPos, size / 3 * w, knobHeight, size / 40);
+    }
+  }
+  translate(0, -size);
+  if (tank === state.rightTank) {
+    drawGauge(-size * 1.5, -size, size, tank.outletPressure, 1e7, "Pa", x, y - size);
+    drawGauge(size * 1.4, -size, size, tank.pressure, 1e7, "Pa", x, y - size);
+  } else {
+    drawGauge(size * 1.5, -size, size, tank.outletPressure, 1e7, "Pa", x, y - size);
+    drawGauge(-size * 1.4, -size, size, tank.pressure, 1e7, "Pa", x, y - size);
+  }
+  pop();
+}
+
+function drawGauge(x, y, diameter, pressure, maxPressure, units, offsetX, offsetY) {
+  push();
+  translate(x, y);
+  const mX = mouseX / relativeSize();
+  const mY = mouseY / relativeSize();
+  if (sqrt((mX - x - offsetX) ** 2 + (mY - y - offsetY) ** 2) < diameter / 2) {
+    scale(4);
+    fill(255);
+    noStroke();
+    circle(0, 0, diameter * 1.6);
+  }
+  const gaugeColor = "rgb(255, 255, 255)";
+  const gaugeOutline = "rgb(110, 110, 110)";
+  const needleColor = "rgb(255, 0, 0)";
+  const needleOutline = "rgb(200, 0, 0)";
+  const pressureFraction = pressure / maxPressure;
+  fill(gaugeColor);
+  stroke(gaugeOutline);
+  strokeWeight(0.15);
+  circle(0, 0, diameter);
+  for (let i = 54; i >= -210; i -= 2.4) {
+    i = round(i * 10) / 10;
+    const x = diameter * cos(radians(i)) / 2;
+    const y = diameter * sin(radians(i)) / 2;
+    stroke(0);
+    noFill();
+    if ((i - 30) % 48 === 0) {
+      strokeWeight(0.05);
+      line(x / 1.4, y / 1.4, x / 1.12, y / 1.12);
+    } else if ((i - 30) % 24 === 0) {
+      strokeWeight(0.04);
+      line(x / 1.3, y / 1.3, x / 1.12, y / 1.12);
+    } else if ((i - 30) % 12 === 0) {
+      strokeWeight(0.03);
+      line(x / 1.23, y / 1.23, x / 1.12, y / 1.12);
+    } else {
+      strokeWeight(0.02);
+      line(x / 1.2, y / 1.2, x / 1.12, y / 1.12);
+    }
+    if ((i - 30) % 48 === 0) {
+      noStroke();
+      fill(0);
+      textSize(1);
+      textAlign(CENTER, CENTER);
+      const textX = diameter / 1.5 * cos(radians(i));
+      const textY = diameter / 1.5 * sin(radians(i));
+      const ratio = round(10 * (1 + ((i - 30) / 240))) / 10;
+      if (units === "Pa") {
+        const P = round(ratio * maxPressure / 1e6);
+        text(P, textX, textY);
+      }
+    }
+  }
+  stroke(needleOutline);
+  strokeWeight(0.05);
+  fill(needleColor);
+  const angle = 240 * pressureFraction - 30;
+  rotate(radians(angle));
+  line(0, 0, -diameter / 3.2, 0);
+  triangle(-diameter / 8, -diameter / 35, -diameter / 8, diameter / 35, -diameter / 3.2, 0);
+  noStroke();
+  fill(150);
+  circle(0, 0, diameter / 15);
+  pop();
+}
+
+function drawConnectingPipe() {
+  push();
+  translate(width / 2, state.leftTank.yCoord - 5);
+  rectMode(CENTER);
+  const pipeColor = "rgb(220, 220, 220)";
+  const pipeOutline = "rgb(100, 100, 100)";
+  const valveBodyColor = "#DDA520";
+  const valveBodyOutline = "#885510";
+  const valveKnobColor = "rgb(80, 80, 80)";
+  const valveKnobOutline = "rgb(50, 50, 50)";
+  fill(pipeColor);
+  stroke(pipeOutline);
+  strokeWeight(0.1);
+  const distanceApart = state.rightTank.xCoord - state.leftTank.xCoord;
+  rect(0, 0, distanceApart, 0.5);
+  rect(-4, 0, 1.5, 1.5);
+  rect(4, 0, 1.5, 1.5);
+  fill(valveBodyColor);
+  stroke(valveBodyOutline);
+  strokeWeight(0.05);
+  beginShape();
+  const vertices = [
+    [-3.5, 1.1],
+    [-3.5, 1.3],
+    [-4, 1.3],
+    [-4, -1.3],
+    [-3.5, -1.3],
+    [-3.5, -1.1],
+    [-1, -1.1],
+    [-1, -2],
+    [-1.5, -2],
+    [-1.5, -2.8],
+    [1.5, -2.8],
+    [1.5, -2],
+    [1, -2],
+    [1, -1.1],
+    [3.5, -1.1],
+    [3.5, -1.3],
+    [4, -1.3],
+    [4, 1.3],
+    [3.5, 1.3],
+    [3.5, 1.1],
+  ]
+  vertices.forEach(v => {
+    vertex(v[0], v[1]);
+  });
+  endShape(CLOSE);
+  fill(200);
+  stroke(120);
+  rect(0, -4, 0.5, 2.5);
+  fill(valveKnobColor);
+  stroke(valveKnobOutline);
+  const t = state.valvePosition * 2;
+  rect(0, -5.25, 4 * abs(cos(t * TWO_PI)) + 1, 1, 0.25);
+  pop();
+}
+
+export function drawAll() {
+  drawConnectingPipe();
+  drawLeftTank();
+  drawRightTank();
 }
