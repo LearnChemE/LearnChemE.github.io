77bd80a16849d5640892ec86ba51e95243690258
NeilTheSeal <neil.hendren@gmail.com>
2025-04-08 15:13:51 -0600

    Chemical equilibrium in the haber process initial commit

---
+++ demos/ChemicalEquilibriumInTheHaberProcess/src/index.js
@@ -0,0 +1,39 @@
+import "bootstrap";
+import "p5";
+import "./style/style.scss";
+import { drawAll } from "./js/draw";
+import { handleInputs } from "./js/inputs";
+import { calcAll } from "./js/calcs";
+// TO DO:
+
+// GLOBAL VARIABLES OBJECT
+window.state = {
+  frameRate: 60,
+  pixelDensity: 4,
+  showButtons: false,
+  hamburgerHasBeenClicked: window.localStorage.getItem("hamburgerHasBeenClicked") === "true",
+};
+
+const containerElement = document.getElementById("p5-container");
+
+window.setup = function() {
+  createCanvas(containerElement.offsetWidth, containerElement.offsetHeight).parent(containerElement);
+  handleInputs();
+  calcAll();
+  pixelDensity(state.pixelDensity);
+  frameRate(state.frameRate);
+};
+
+window.draw = function() {
+  window.width = 150;
+  window.height = 120;
+  scale(relativeSize());
+  background(255);
+  drawAll();
+};
+
+window.windowResized = () => {
+  resizeCanvas(containerElement.offsetWidth, containerElement.offsetHeight);
+}
+
+window.relativeSize = () => containerElement.offsetWidth / 150;
