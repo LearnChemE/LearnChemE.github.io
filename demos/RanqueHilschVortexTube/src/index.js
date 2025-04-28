f27751d7477a09c68db16d96378d19977efb75d6
NeilTheSeal <neil.hendren@gmail.com>
2025-04-28 13:22:31 -0600

    Ranque Hilsch Vortex Tube initial commit

---
+++ demos/RanqueHilschVortexTube/src/index.js
@@ -0,0 +1,52 @@
+import "bootstrap";
+import "p5";
+import "./style/style.scss";
+import { drawAll } from "./js/draw";
+import { handleInputs } from "./js/inputs";
+import { calcAll } from "./js/calcs";
+
+// GLOBAL VARIABLES OBJECT
+window.state = {
+  frameRate: 60,
+  pixelDensity: 4,
+  showButtons: false,
+  hamburgerHasBeenClicked: window.localStorage.getItem("hamburgerHasBeenClicked") === "true",
+  canvasSize: [150, 100],
+  P: Number(document.getElementById("feed-pressure-slider").value), // bar
+  z: Number(document.getElementById("fraction-feed-slider").value), // fraction of feed in cold stream
+};
+
+const containerElement = document.getElementById("p5-container");
+
+window.setup = function() {
+  sizeContainer();
+  createCanvas(containerElement.offsetWidth, containerElement.offsetHeight).parent(containerElement);
+  handleInputs();
+  calcAll();
+  pixelDensity(state.pixelDensity);
+  frameRate(state.frameRate);
+};
+
+window.draw = function() {
+  window.width = state.canvasSize[0];
+  window.height = state.canvasSize[1];
+  scale(relativeSize());
+  background(255);
+  calcAll();
+  drawAll();
+};
+
+window.windowResized = () => {
+  resizeCanvas(containerElement.offsetWidth, containerElement.offsetHeight);
+}
+
+window.relativeSize = () => containerElement.offsetWidth / state.canvasSize[0];
+
+function sizeContainer() {
+  containerElement.style.width = `calc(100vw - 10px)`;
+  containerElement.style.maxWidth = `calc(calc(100vh - 10px) * ${state.canvasSize[0]} / ${state.canvasSize[1]})`;
+  containerElement.style.height = `calc(calc(100vw - 10px) * ${state.canvasSize[1]} / ${state.canvasSize[0]})`;
+  containerElement.style.maxHeight = `calc(100vh - 10px)`;
+}
+
+require("./js/events.js");
