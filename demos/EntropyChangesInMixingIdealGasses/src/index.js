c3c8e46210ef236fba6e55fcb28494ac52b7003e
NeilTheSeal <neil.hendren@gmail.com>
2025-05-02 10:02:11 -0600

    Add entropy changes in mixing ideal gasses

---
+++ demos/EntropyChangesInMixingIdealGasses/src/index.js
@@ -0,0 +1,52 @@
+import "bootstrap";
+import "p5";
+import "./style/style.scss";
+import { drawAll } from "./js/draw.js";
+import { handleInputs } from "./js/inputs.js";
+import { calcAll, setDefaults } from "./js/calcs.js";
+
+// GLOBAL VARIABLES OBJECT
+window.state = {
+  frameRate: 60,
+  pixelDensity: 4,
+  showButtons: false,
+  hamburgerHasBeenClicked: window.localStorage.getItem("hamburgerHasBeenClicked") === "true",
+  canvasSize: [150, 120],
+};
+
+const containerElement = document.getElementById("p5-container");
+
+window.setup = function() {
+  sizeContainer();
+  createCanvas(containerElement.offsetWidth, containerElement.offsetHeight).parent(containerElement);
+  handleInputs();
+  setDefaults();
+  calcAll();
+  pixelDensity(state.pixelDensity);
+  frameRate(state.frameRate);
+};
+
+window.draw = function() {
+  window.width = state.canvasSize[0];
+  window.height = state.canvasSize[1];
+  window.mX = mouseX / relativeSize();
+  window.mY = mouseY / relativeSize();
+  scale(relativeSize());
+  background(255);
+  drawAll();
+};
+
+window.windowResized = () => {
+  resizeCanvas(containerElement.offsetWidth, containerElement.offsetHeight);
+}
+
+window.relativeSize = () => containerElement.offsetWidth / 150;
+
+function sizeContainer() {
+  containerElement.style.width = `calc(100vw - 10px)`;
+  containerElement.style.maxWidth = `calc(calc(100vh - 10px) * ${state.canvasSize[0]} / ${state.canvasSize[1]})`;
+  containerElement.style.height = `calc(calc(100vw - 10px) * ${state.canvasSize[1]} / ${state.canvasSize[0]})`;
+  containerElement.style.maxHeight = `calc(100vh - 10px)`;
+}
+
+require("./js/events.js");
