55e3eb40c71c7445521d634c1a08122b321c6188
ComputerDJ <jduncode@gmail.com>
2025-05-13 14:50:42 -0600

    some of the cylinder moving

--- demos/GasPhaseEquilibriumConstantPV/js/calcs.js
+++ demos/GasPhaseEquilibriumConstantPV/js/calcs.js
@@ -6,33 +6,31 @@
 */
 
 export function calcAll() {
-    //bring in r from html
-    const coeffB = document.querySelector('input[name="plot"]:checked');
-    let r = eval(coeffB.value);
+  //bring in r from html
+  const coeffB = document.querySelector('input[name="plot"]:checked');
+  let r = eval(coeffB.value);
 
-    function totalMoles(x) {
-        return z.initialA + z.molesInerts + x * (r - 1);
-    }
+  function totalMoles(x) {
+    return z.initialA + z.molesInerts + x * (r - 1);
+  }
 
-    function pressureConstVolumeCase(x) {
-        return (totalMoles(x) * z.R * z.temperature) / z.volumeConstVolumeCase;
-    }
+  function pressureConstVolumeCase(x) {
+    return (totalMoles(x) * z.R * z.temperature) / z.volumeConstVolumeCase;
+  }
 
-    function volumeConstPressureCase(x) {
-        return (
-            (totalMoles(x) * z.R * z.temperature) / z.pressureConstPressureCase
-        );
-    }
+  function volumeConstPressureCase(x) {
+    return (totalMoles(x) * z.R * z.temperature) / z.pressureConstPressureCase;
+  }
 
-    function equilMolsA(x) {
-        return z.initialA - x;
-    }
+  function equilMolsA(x) {
+    return z.initialA - x;
+  }
 
-    function equilMolsB(x) {
-        return r * x;
-    }
+  function equilMolsB(x) {
+    return r * x;
+  }
 
-    /*
+  /*
   z.totalMoles = z.initialA + z.molesInerts + x*(r-1);
   z.pressureConstVolumeCase = z.totalMoles*z.R*z.temperature/z.volumeConstVolumeCase;
   z.volumeConstPressureCase = z.totalMoles*z.R*z.temperature/z.pressureConstVolumeCase;
@@ -40,62 +38,48 @@ export function calcAll() {
   z.equilMolsB = r*x;
   */
 
-    function extentReactionEQConstPressureCase(x) {
-        return (
-            z.kEQ -
-            ((equilMolsB(x) * z.pressureConstPressureCase) / totalMoles(x)) **
-                r /
-                ((equilMolsA(x) * z.pressureConstPressureCase) / totalMoles(x))
-        );
+  function extentReactionEQConstPressureCase(x) {
+    return (
+      z.kEQ - ((equilMolsB(x) * z.pressureConstPressureCase) / totalMoles(x)) ** r / ((equilMolsA(x) * z.pressureConstPressureCase) / totalMoles(x))
+    );
+  }
+
+  function extentReactionEQConstVolumeCase(x) {
+    return (
+      z.kEQ - ((equilMolsB(x) * pressureConstVolumeCase(x)) / totalMoles(x)) ** r / ((equilMolsA(x) * pressureConstVolumeCase(x)) / totalMoles(x))
+    );
+  }
+
+  //root solver
+  function findRoot(f, a, b) {
+    let c = (a + b) / 2;
+
+    while (Math.abs(f(c)) > 0.0000001) {
+      if (f(a) * f(c) < 0) {
+        b = c;
+      } else {
+        a = c;
+      }
+
+      c = (a + b) / 2;
     }
 
-    function extentReactionEQConstVolumeCase(x) {
-        return (
-            z.kEQ -
-            ((equilMolsB(x) * pressureConstVolumeCase(x)) / totalMoles(x)) **
-                r /
-                ((equilMolsA(x) * pressureConstVolumeCase(x)) / totalMoles(x))
-        );
-    }
+    return c.toFixed(3);
+  }
 
-    //root solver
-    function findRoot(f, a, b) {
-        let c = (a + b) / 2;
+  //z.extentConstPressureCase = z.pressureConstPressureCase;s
 
-        while (Math.abs(f(c)) > 0.0000001) {
-            if (f(a) * f(c) < 0) {
-                b = c;
-            } else {
-                a = c;
-            }
+  z.extentConstPressureCase = findRoot(extentReactionEQConstPressureCase, 0, 4.999999);
+  z.molAForConstPressureSelection = equilMolsA(z.extentConstPressureCase);
+  z.molBForConstPressureSelection = equilMolsB(z.extentConstPressureCase);
+  z.volumeConstPressureCase = volumeConstPressureCase(z.extentConstPressureCase);
 
-            c = (a + b) / 2;
-        }
+  z.extentConstVolumeCase = findRoot(extentReactionEQConstVolumeCase, 0, 4.999999);
+  z.molAForConstVolumeSelection = equilMolsA(z.extentConstVolumeCase);
+  z.molBForConstVolumeSelection = equilMolsB(z.extentConstVolumeCase);
+  z.pressureConstVolumeCase = pressureConstVolumeCase(z.extentConstVolumeCase);
 
-        return c.toFixed(3);
-    }
-
-    //z.extentConstPressureCase = z.pressureConstPressureCase;s
-
-    z.extentConstPressureCase = findRoot(
-        extentReactionEQConstPressureCase,
-        0,
-        4.999999
-    );
-    z.molAForConstPressureSelection = equilMolsA(z.extentConstPressureCase);
-    z.molBForConstPressureSelection = equilMolsB(z.extentConstPressureCase);
-    z.volumeConstPressureCase = volumeConstPressureCase(
-        z.extentConstPressureCase
-    );
-
-    z.extentConstVolumeCase = findRoot(
-        extentReactionEQConstVolumeCase,
-        0,
-        4.999999
-    );
-    z.molAForConstVolumeSelection = equilMolsA(z.extentConstVolumeCase);
-    z.molBForConstVolumeSelection = equilMolsB(z.extentConstVolumeCase);
-    z.pressureConstVolumeCase = pressureConstVolumeCase(
-        z.extentConstVolumeCase
-    );
+  let totalCylinderVolume = 2.25;
+  z.cylinderLiveVolumeFractionConstantPressureCase = z.volumeConstPressureCase / totalCylinderVolume;
+  z.cylinderLiveVolumeFractionConstantVolumeCase = z.volumeConstVolumeCase / totalCylinderVolume;
 }
--- demos/GasPhaseEquilibriumConstantPV/js/main.js
+++ demos/GasPhaseEquilibriumConstantPV/js/main.js
@@ -7,38 +7,41 @@ const mainWidth = 1280;
 const mainHeight = 720;
 // Declare global variables which are available to all files
 window.z = {
-    //graph and window variables
-    width: mainWidth,
-    height: mainWidth,
+  //graph and window variables
+  width: mainWidth,
+  height: mainWidth,
 
-    //slider inputs
-    pressureConstPressureCase: 0.7,
-    volumeConstPressureCase: 0.74,
-    pressureConstVolumeCase: 0.7,
-    volumeConstVolumeCase: 0.74,
-    molesInerts: 0.0000000001,
-    R: 0.00008314,
-    temperature: 900,
-    totalMoles: 0,
-    initialA: 5,
-    coeffBValue: 2,
-    result1: 0,
-    c: 0,
-    equilMolsA: 0,
-    equilMolsB: 0,
-    extentConstPressureCase: 0,
-    extentConstVolumeCase: 0,
-    kEQ: 0.5,
+  //slider inputs
+  pressureConstPressureCase: 0.7,
+  volumeConstPressureCase: 0.74,
+  pressureConstVolumeCase: 0.7,
+  volumeConstVolumeCase: 0.74,
+  molesInerts: 0.0000000001,
+  R: 0.00008314,
+  temperature: 900,
+  totalMoles: 0,
+  initialA: 5,
+  coeffBValue: 2,
+  result1: 0,
+  c: 0,
+  equilMolsA: 0,
+  equilMolsB: 0,
+  extentConstPressureCase: 0,
+  extentConstVolumeCase: 0,
+  kEQ: 0.5,
 
-    //3D cylinder parameters
-    cylRadius: 100,
-    cylHeight: 300,
+  //3D cylinder parameters
+  cylRadius: 100,
+  cylHeight: 300,
 
-    molAForConstPressureSelection: 3.0,
-    molBForConstPressureSelection: 4.1,
+  cylinderLiveVolumeFractionConstantPressureCase: 0,
+  cylinderLiveVolumeFractionConstantVolumeCase: 0,
 
-    molAForConstVolumeSelection: 3.1,
-    molBForConstVolumeSelection: 3.9,
+  molAForConstPressureSelection: 3.0,
+  molBForConstPressureSelection: 4.1,
+
+  molAForConstVolumeSelection: 3.1,
+  molBForConstVolumeSelection: 3.9,
 };
 
 // Load the other scripts (except calcs.js, which is imported in draw.js).
