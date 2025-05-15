cc7651755647e66716eb15c84007860b2bdc56a2
NeilTheSeal <neil.hendren@gmail.com>
2025-05-15 14:33:22 -0600

    Add single effect evaporator digital experiment

---
+++ lab-experiments/single_effect_evaporator/_dev/webpack.config.js
@@ -0,0 +1,126 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const module_rules = [{
+    test: /\.ttf|\.wav$/i,
+    loader: 'file-loader',
+    options: {
+      name: 'assets/[name].[ext]',
+    },
+  },
+  {
+    test: /\.(sa|sc|c)ss$/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      "css-loader",
+      "sass-loader",
+    ]
+  },
+  {
+    test: /\.svg$/i,
+    use: ["to-string-loader", "html-loader"]
+  },
+  {
+    test: /\.(docx|pdf)$/i,
+    loader: 'file-loader',
+    options: {
+      name: 'assets/[name].[ext]',
+    },
+  }
+];
+
+const html_options = {
+  title: "Single-Effect Evaporator Digital Lab",
+  filename: "index.html",
+  template: path.resolve(__dirname, '../src/html/index.html'),
+  scriptLoading: "blocking",
+  hash: true,
+  meta: {
+    "viewport": "width=device-width, initial-scale=1, shrink-to-fit=no",
+    "keywords": "LearnChemE, chemical engineering, engineering, simulation",
+    "author": "Venkatesh Mopidevi",
+    "application-name": "Single-Effect Evaporator Digital Lab",
+    "description": "A digital laboratory experiment in which students explore the evaporation of a sugar solution using a single-effect evaporator."
+  },
+}
+
+let config = {
+  stats: 'errors-only',
+  entry: {
+    index: path.resolve(__dirname, '../src/index.js'),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, '../dist'),
+      watch: true,
+    },
+    hot: false,
+    liveReload: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+      logging: 'error',
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin(html_options),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        '**/*',
+        '!assets/**',
+      ],
+    }),
+  ],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, '../dist'),
+    clean: false,
+  },
+  module: {
+    rules: module_rules
+  },
+  experiments: {
+    futureDefaults: true,
+  },
+  optimization: null
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === "production") {
+
+    config.optimization = {
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+          minify: TerserPlugin.uglifyJsMinify
+        }),
+      ],
+      moduleIds: 'size',
+      chunkIds: 'total-size',
+      removeAvailableModules: true,
+    }
+
+  } else {
+
+    config.optimization = {
+      minimize: false,
+      moduleIds: 'named',
+      chunkIds: 'named',
+      removeAvailableModules: false,
+      realContentHash: false,
+    }
+
+    config.devtool = 'source-map';
+
+  };
+
+  return config;
+}
