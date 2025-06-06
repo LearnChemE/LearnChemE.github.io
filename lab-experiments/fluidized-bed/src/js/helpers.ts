5856c23b0b6c285498b11cab28a9d25bb2c05cbe
drew <drsm1370@colorado.edu>
2025-06-06 12:07:59 -0600

    added some of my gl helpers

--- lab-experiments/fluidized-bed/src/js/helpers.ts
+++ lab-experiments/fluidized-bed/src/js/helpers.ts
@@ -1,6 +1,13 @@
+import { VertexAttribute } from "../types";
 import State from "./state";
 
-// Constain x to the bounds [min, max]
+/**
+ * Constain x to the bounds [min, max]
+ * @param x Number to constrain
+ * @param min minimum value for new x
+ * @param max maximum value for new x
+ * @returns constrained value for x
+ */
 export function constrain(x: number, min: number, max: number) {
     if (min > max) {
         throw new Error("Bad range for constrain: min must be less than max");
@@ -12,13 +19,30 @@ export function constrain(x: number, min: number, max: number) {
     return x;
 }
 
-// Lerp function
+/**
+ * Lerp function
+ * @param a min lerp bound
+ * @param b max lerp bound
+ * @param t interpolant
+ * @returns linearly interpolated value
+ */
 export function lerp(a: number, b: number, t: number) {
     return a + (b - a) * t;
 }
 
-// Rescale x from scale of a to b to scale of c to d 
-// 6th argument optionally sets whether to constrain to bounds
+// 
+
+/**
+ * Rescale x from scale of a to b to scale of c to d
+ * 6th argument optionally sets whether to constrain to bounds
+ * @param x number to be rescaled
+ * @param a min of original scale
+ * @param b max of original scale
+ * @param c min of new scale
+ * @param d max of new scale
+ * @param constrain sets whether to constrain new number to bounds of c and d (default false)
+ * @returns Number with new scale
+ */
 export function rescale(x: number, a: number, b: number, c: number, d: number, constrain: boolean=false) {
     if (a > b || c > d) throw new Error("Rescale bounds inverted");
 
@@ -79,4 +103,136 @@ export function smoothLerp(duration: number, updateCallback: (t: number) => void
         // Start the animation
         requestAnimationFrame(animate);
     });
+}
+
+/**
+ * Create a WebGL shader module using vertex and fragment sources
+ * @param gl WebGL context
+ * @param vert Vertex shader source code
+ * @param frag Fragment shader source code
+ * @returns WebGLProgram if successfully compiled; null if errors occurred
+ */
+export function CreateShader(gl: WebGL2RenderingContext, vert: string, frag: string): WebGLProgram | null {
+  // Create the shader objects
+  let vertShader: WebGLShader = gl.createShader(gl.VERTEX_SHADER);
+  let fragShader: WebGLShader = gl.createShader(gl.FRAGMENT_SHADER);
+  // Specify the sources
+  gl.shaderSource(vertShader, vert);
+  gl.shaderSource(fragShader, frag);
+  // Compile the shaders
+  gl.compileShader(vertShader);
+  gl.compileShader(fragShader);
+
+  // Check compile status
+  if (!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS)) {
+    console.warn(
+      "Vertex Shader Compilation Error: " + gl.getShaderInfoLog(vertShader)
+    );
+    return;
+  }
+  if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
+    console.warn(
+      "Fragment Shader Compilation Error: " + gl.getShaderInfoLog(fragShader)
+    );
+    return;
+  }
+
+  // Create shader program
+  let shdrProg = gl.createProgram();
+  // Attach shaders
+  gl.attachShader(shdrProg, vertShader);
+  gl.attachShader(shdrProg, fragShader);
+  // Link
+  gl.linkProgram(shdrProg);
+
+  // Check link status
+  if (!gl.getProgramParameter(shdrProg, gl.LINK_STATUS)) {
+    console.warn("Program Linking Error: " + gl.getProgramInfoLog(shdrProg));
+    return;
+  }
+
+  // Return
+  return shdrProg;
+}
+
+/**
+ * Create a VAO from vertices and indices.
+ * @param gl WebGL context
+ * @param vrts List of vertices
+ * @param idxs List of indices
+ * @param shdr Shader program to assign attributes for
+ * @param attribs Array of attribute descriptors to be assigned
+ * @returns 
+ */
+export function CreateVao(gl: WebGL2RenderingContext, vrts: Float32Array, idxs: Float32Array, shdr: WebGLProgram, attribs: Array<VertexAttribute>) {
+    // Create VAO
+    const vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
+
+    // Vertex data
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, vrts, gl.STATIC_DRAW);
+    // Index data
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, idxs, gl.STATIC_DRAW);
+
+    // Vertex attributes
+    for (let attr of attribs) {
+        // Find the location of the attribute
+        const loc = gl.getAttribLocation(shdr, attr.name);
+        // Check for errors
+        if (loc !== -1) {
+            // Tell GL what the attribute is
+            gl.vertexAttribPointer(loc, attr.size, attr.type, attr.normalized, attr.stride, attr.offset);
+            // Enable the attribute
+            gl.enableVertexAttribArray(loc);
+        } 
+        else {
+            // Couldn't find attribute location
+            console.warn(`Attribute "${attr.name}" not found in shader "${shdr}".`);
+        }
+    }
+
+    // Clean and return
+    gl.bindVertexArray(null);
+    return vao;
+}
+
+/**
+ * Create a UBO object
+ * @param gl WebGL rendering context
+ * @param size Size of the buffer
+ * @param shaders Array of webGL shader modules that refer to this UBO
+ * @param name Name of the UBO
+ * @param binding UBO block binding
+ * @returns 
+ */
+export function CreateUBO(gl: WebGL2RenderingContext, size: number, shaders: Array<WebGLProgram>, name: string, binding: number) {
+    // Create buffer
+    const buffer = gl.createBuffer();
+    gl.bindBuffer(gl.UNIFORM_BUFFER, buffer);
+
+    // Allocate memory
+    gl.bufferData(gl.UNIFORM_BUFFER, size, gl.DYNAMIC_DRAW);
+
+    // Assign binding point to each shader
+    for (const shader of shaders) {
+        let uniformBlockIndex = gl.getUniformBlockIndex(shader, name);
+        gl.uniformBlockBinding(shader, uniformBlockIndex, binding);
+    }
+
+    // Bind buffer to binding point
+    gl.bindBufferBase(gl.UNIFORM_BUFFER, binding, buffer);
+    return buffer;
+}
+
+/**
+ * Set data for uniform buffer
+ * @param gl WebGL rendering context
+ * @param ubo Target buffer to be set
+ * @param data Data to set to buffer
+ */
+export function SetUBO(gl: WebGL2RenderingContext, ubo: WebGLBuffer, data: Float32Array) {
+    gl.bindBuffer(gl.UNIFORM_BUFFER, ubo);
+    gl.bufferSubData(gl.UNIFORM_BUFFER, 0, data);
 }
--- lab-experiments/fluidized-bed/src/types/index.ts
+++ lab-experiments/fluidized-bed/src/types/index.ts
@@ -3,3 +3,4 @@ export * from './ani-types';
 export * from './bend-direction';
 export * from './manometer';
 export * from './beaker';
+export * from './gltypes';
