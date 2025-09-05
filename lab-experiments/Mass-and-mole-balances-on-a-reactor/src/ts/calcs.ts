74f4dc5aff4e35b39f4b890eb6cb755d1cea24c4
drew <drsm1370@colorado.edu>
2025-09-05 13:13:21 -0600

    calculations

---
+++ lab-experiments/Mass-and-mole-balances-on-a-reactor/src/ts/calcs.ts
@@ -0,0 +1,90 @@
+import type { Signal } from "../classes/Signal";
+
+const V = 1; // L
+const P = 1; // atm
+const RHO = 0.8; // g / mL
+const R = 0.08206; // L atm / mol / K
+
+const MW_Propanol = 60.1; // g/mol
+const MW_Water = 18;
+// const MW_Hydrogen = 2.02;
+// const MW_Propene = 42.08;
+const MW_Propanal = 58.08;
+
+export function Reactor(inFlow: Signal<number>, temp: Signal<number>) {
+    var flowrate = inFlow.get();
+    var temperature = temp.get();
+
+    inFlow.subscribe((flow: number) => {
+        flowrate = flow;
+        calcAll(flowrate, temperature);
+    });
+    temp.subscribe((t: number) => {
+        temperature = t;
+        calcAll(flowrate, temperature);
+    });
+
+    // TODO: Call CalcAll somewhere outside so it will not be called more than once per frame
+}
+
+
+type ProductStream = {
+    liquidFlowrate: number,
+    gasFlowrate: number
+};
+type ArrhenniusConstants = {
+    k0: number,
+    ea: number
+}
+
+const rxn1: ArrhenniusConstants = {
+    k0: 20,
+    ea: 2000
+}
+const rxn2: ArrhenniusConstants = {
+    k0: 3000,
+    ea: 4000
+}
+
+function arrhennius(cs: ArrhenniusConstants, T: number) {
+    return cs.k0 * Math.exp(-cs.ea / T);
+}
+
+/**
+ * Calculate the reactor and exiting compositions
+ * @param flow Liquid flowrate of feed (mL/s)
+ * @param temp Temperature of reactor (C)
+ */
+function calcAll(flow: number, temp: number): ProductStream {
+    const T = temp + 273; // K
+    const mdot_feed = flow * RHO; // g / s
+    const ndot_feed = mdot_feed / MW_Propanol; // mol / s
+    const Qvap = ndot_feed * R * (T + 273) / P; // Volumetric flowrate of gas (L/s)
+
+    // Reaction params
+    const C0 = ndot_feed / Qvap; // Concentration of propanol entering reactor (M)
+    const tau = Qvap / V; // Reaction spacetime
+
+    // Reaction constants
+    const k1 = arrhennius(rxn1, T);
+    const k2 = arrhennius(rxn2, T);
+
+    // Calculate the composition after the rxn
+    const rxn = -C0 * (1 - Math.exp(-(k1 + k2) * tau));
+    const conc_propanol = C0 + rxn;
+    const conc_propene  = rxn * k1 / (k1 + k2);
+    const conc_propanal = rxn - conc_propene;
+    const c_tot = conc_propanol + 2 * conc_propene + 2 * conc_propanal; // M
+
+    // Put in terms of moles
+    const n_out = c_tot * Qvap; // mol / s
+    const m_liq = n_out / c_tot * (MW_Propanol * conc_propanol + MW_Water * conc_propene + MW_Propanal * conc_propanal);
+    const n_vap = n_out / c_tot * (conc_propene + conc_propanal);
+
+    // Finally, convert to volumetric flowrates
+    const liq = m_liq / RHO;
+    const vap = n_vap * R * 298.15 / P * 1000;
+    console.log(`Inlet flow: ${flow} mL/s @ ${temp} C\n  Liq: ${liq} mL/s\n  Vap: ${vap} mL/s`);
+
+    return { liquidFlowrate: liq, gasFlowrate: vap };
+}
