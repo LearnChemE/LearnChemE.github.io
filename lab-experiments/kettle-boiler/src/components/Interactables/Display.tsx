faa622f3b2ca976f14212afc70f76419daf82644
Drew Smith <drew.smith@colorado.edu>
2025-09-25 17:45:58 -0600

    made everything modular

--- lab-experiments/kettle-boiler/src/components/Interactables/Display.tsx
+++ lab-experiments/kettle-boiler/src/components/Interactables/Display.tsx
@@ -1,8 +1,33 @@
 import type { Component } from "solid-js";
 
-export const Display: Component = () => {
+interface DisplayProps {
+  x: number;
+  y: number;
+};
+
+export const Display: Component<DisplayProps> = ({ x, y }) => {
   return (
-    <></>
+    <g id="display">
+      <rect
+        x={`${x}`}
+        y={`${y}`}
+        width="66"
+        height="33"
+        rx="3.5"
+        fill="#888888"
+        stroke="black"
+      />
+      <rect
+        id="condTempScreen"
+        x={`${x + 3}`}
+        y={`${y + 3}`}
+        width="60"
+        height="27"
+        rx="3.5"
+        fill="#111111"
+        stroke="black"
+      />
+    </g>
 )};
 
 export default Display;
--- lab-experiments/kettle-boiler/src/components/Interactables/Waterfall.tsx
+++ lab-experiments/kettle-boiler/src/components/Interactables/Waterfall.tsx
@@ -1,8 +1,17 @@
 import type { Component } from "solid-js";
 
-export const Waterfall: Component = () => {
+export interface WaterfallProps {
+  d: string;
+};
+
+export const Waterfall: Component<WaterfallProps> = ({ d }) => {
   return (
-    <></>
+  <path
+    id="condFall"
+    d={d}
+    fill="#3B8CCF"
+    fill-opacity="0.6"
+  />
 )};
 
 export default Waterfall;
