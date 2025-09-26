8aaca52787e158ab457e77fd108e4f0f40ab362d
Drew Smith <drew.smith@colorado.edu>
2025-09-25 18:07:04 -0600

    zoom and drag

--- lab-experiments/kettle-boiler/src/App.tsx
+++ lab-experiments/kettle-boiler/src/App.tsx
@@ -1,10 +1,18 @@
-// import { createSignal } from 'solid-js'
+import { onMount } from 'solid-js';
 import './App.css'
 import { Apparatus } from './components/Apparatus';
 import { HamburgerMenu } from './components/Hamburger/Hamburger';
 import worksheet from './media/testMe.txt'
+import { initSvgDrag, initSvgZoom } from './ts/helpers';
 
 function App() {
+
+  onMount(() => {
+    // Initialize SVG zooming
+    initSvgZoom();
+    // Initialize SVG dragging with exempt element IDs
+    initSvgDrag();
+  });
 
   return (
     <>
