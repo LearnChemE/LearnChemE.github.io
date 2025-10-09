4c998cbd24fc2228e01afcaf695b2448f9126cf3
Drew Smith <drew.smith@colorado.edu>
2025-10-09 12:07:58 -0600

    project layout

--- lab-experiments/cell-sedimentation/src/App.tsx
+++ lab-experiments/cell-sedimentation/src/App.tsx
@@ -1,33 +1,13 @@
 import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { HamburgerMenu } from './components/Hamburger/Hamburger'
+import { AboutText, DirectionsText } from './components/Modal/modals'
 
 function App() {
-  const [count, setCount] = createSignal(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
+      <HamburgerMenu path="" downloadName="" Directions={DirectionsText} About={AboutText} />
     </>
   )
 }
