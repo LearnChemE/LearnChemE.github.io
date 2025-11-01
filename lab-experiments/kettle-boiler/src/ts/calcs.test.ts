6b60328e85c64ee65b6b7ade648a8c9b2f958c70
Drew Smith <drew.smith@colorado.edu>
2025-11-01 16:34:50 -0600

    fixed bugs

--- lab-experiments/kettle-boiler/src/ts/calcs.test.ts
+++ lab-experiments/kettle-boiler/src/ts/calcs.test.ts
@@ -1,6 +1,20 @@
-import { expect, test } from 'vitest';
-import { antoines } from './calcs';
+import { expect, test, describe, it } from 'vitest';
+import { antoines, dHvap } from './calcs';
+import { plot, type PlotData } from 'nodeplotlib';
 
 test('antoines returns normal boiling pt', async () => {
     expect(antoines(373.15)).approximately(1, .05);
+});
+
+async function plotFn(xRange: number[], fn: (x: number) => number, options?: Partial<PlotData>) {
+    const y = xRange.map(fn);
+    plot([{ x: xRange, y, type: 'scatter', ...options }]);
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // Arbitrary timeout to give client time to load
+}
+
+describe('function visualization', () => {
+    it('plots dHvap', async () => {
+        const x = Array.from({ length: 200 }, (_,i) => i * 2);
+        await plotFn(x, dHvap);
+    });
 });
