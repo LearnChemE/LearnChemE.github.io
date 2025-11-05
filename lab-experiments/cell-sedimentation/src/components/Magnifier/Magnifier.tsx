64123fc1b98a76c1f00784e16e08ec7fabb27b4e
Drew Smith <drew.smith@colorado.edu>
2025-11-04 18:12:11 -0700

    working magnifier tracking

--- lab-experiments/cell-sedimentation/src/components/Magnifier/Magnifier.tsx
+++ lab-experiments/cell-sedimentation/src/components/Magnifier/Magnifier.tsx
@@ -1,6 +1,7 @@
-import { createEffect, createSignal, onMount, type Accessor, type Component } from "solid-js";
+import { createEffect, createSignal, For, onMount, type Accessor, type Component } from "solid-js";
 import "./Magnifier.css"
 import MagnifierCanvas from "./MagnifierCanvas";
+import { constrain } from "../../ts/helpers";
 
 interface MagnifierProps {
     magnifying: Accessor<boolean>
@@ -8,10 +9,28 @@ interface MagnifierProps {
 
 export const Magnifier: Component<MagnifierProps> = ({ magnifying }) => {
     const [coord, setCoord] = createSignal({ x: 0, y: 0 });
+    const [vial, setVial] = createSignal(0);
 
     const followMouse = (evt: MouseEvent | Touch) => {
+        // Set new coordinates for the magnifier
         const bds = document.getElementById("root")!.getBoundingClientRect();
-        const newCoord = { x: evt.clientX - bds.x, y: evt.clientY - bds.y };
+        // Y
+        const topBd =  8.5;
+        const botBd = 90.5;
+        // X
+        const vialWidth = .086;
+        const lBd = .328 - vialWidth / 2;
+        const x_cnv = (evt.clientX - bds.x) / bds.width;
+        const y_cnv = (evt.clientY - bds.y) / bds.height;
+        // Determine vial
+        const vial = constrain(Math.floor((x_cnv - lBd) / vialWidth), 0, 4);
+        console.log(vial)
+
+        // Calculate new coords
+        const newCoord = {
+            x: 32.8 + vial * vialWidth * 100, 
+            y: constrain(y_cnv * 100, topBd, botBd)
+        };
         setCoord(newCoord);
     }
 
@@ -21,7 +40,7 @@ export const Magnifier: Component<MagnifierProps> = ({ magnifying }) => {
     });
 
     return (<>
-        { magnifying() && <div class="lens" style={`left: ${coord().x}px; top: ${coord().y}px`}>
+        { magnifying() && <div class="lens" style={`left: ${coord().x}%; top: ${coord().y}%`}>
             <MagnifierCanvas />
         </div> }
     </>);
