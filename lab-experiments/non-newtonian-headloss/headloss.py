5135a8405ae79154802e071c6ecd9e3e3ccbaf0e
drew <drsm1370@colorado.edu>
2026-08-10 09:10:35 -0700

    python model

--- lab-experiments/non-newtonian-headloss/headloss.py
+++ lab-experiments/non-newtonian-headloss/headloss.py
@@ -8,43 +8,43 @@ d = 2 * r  # tube diameter (cm)
 tubeArea = np.pi * r ** 2
 v = V / tubeArea  # cm/s velocity
 
-# Water properties
+fluids = [
+    { 'name': 'Water', 'rho': 1.0, 'n': 1.0, 'K': 0.0089, 't0': 0.0 },
+    { 'name': 'Ketchup', 'rho': 1.1, 'n': 0.228, 'K': 19.34, 't0': 0.0 },
+    { 'name': 'Cornstarch', 'rho': 1.2, 'n': 1.75, 'K': 0.001, 't0': 0.0 }
+]
 
-# Ketchup
-rho = 1.1  # g/cm^3
-n = 0.228  # flow behavior index
-K = 19.34  # Pa·s^n
-t0 = 0.0  # Pa
+for fluid in fluids:
+    rho = fluid['rho']
+    n = fluid['n']
+    K = fluid['K']
+    t0 = fluid['t0']
 
-# Cornstarch
-# rho = 1.2  # g/cm^3
-# n = 1.72  # flow behavior index
-# K = 0.13  # Pa·s^n
-# t0 = 0.0  # Pa
+    Re = rho * v ** (2 - n) * d ** n / K / ((3 * n + 1) / 4 / n) ** n / 8 ** (n - 1) + 1e-10 # Reynolds number
+    Re_crit = 2200 * n ** -0.15 + 100 * n ** -2 # critical Reynolds number for transition to turbulence
 
-Re = rho * v ** (2 - n) * d ** n / K / ((3 * n + 1) / 4 / n) ** n / 8 ** (n - 1) + 1e-10 # Reynolds number
-Re_crit = 2200 * n ** -0.15 + 100 * n ** -2 # critical Reynolds number for transition to turbulence
+    laminar = Re < Re_crit
 
-laminar = Re < Re_crit
+    # Calculate laminar pressure drop per unit length
+    shearRate = (3 * n + 1) / 4 / n * (8 * v) / d # s^-1
+    shearStress = t0 + K * np.power(shearRate, n) # Pa
+    dPdL_lam = 4 * shearStress / d # Pa/cm
+    # Calculate turbulent pressure drop per unit length
+    num = 42 * n ** 1.4 * (n ** 1.4 + 2) + 0.33
+    den = n ** 1.4 + 0.211
+    a = num / den
+    b = (1 + 413.6 * n) ** -0.23
+    print(a, b)
+    f = (a * Re) ** -b # friction factor
+    Pdyn = 0.5 * rho * v ** 2 # dynamic pressure (Pa)
+    dPdL_tur = f * Pdyn / d # Pa/cm
 
-# Calculate laminar pressure drop per unit length
-shearRate = (3 * n + 1) / 4 / n * (8 * v) / d # s^-1
-shearStress = t0 + K * np.power(shearRate, n) # Pa
-dPdL_lam = 4 * shearStress / d # Pa/cm
-# Calculate turbulent pressure drop per unit length
-num = 42 * n ** 1.4 * (n ** 1.4 + 2) + 0.33
-den = n ** 1.4 + 0.211
-a = num / den
-b = (1 + 413.6 * n) ** -0.23
-print(a, b)
-f = (a * Re) ** -b # friction factor
-Pdyn = 0.5 * rho * v ** 2 # dynamic pressure (Pa)
-dPdL_tur = f * Pdyn / d # Pa/cm
+    dPdL = np.where(laminar, dPdL_lam, dPdL_tur)
+    dhdL = dPdL / (rho * 9810) * 100 # head loss per unit length (cm/cm)
 
-dPdL = np.where(laminar, dPdL_lam, dPdL_tur)
-dhdL = dPdL / (rho * 9810) * 100 # head loss per unit length (cm/cm)
+    plt.plot(V, dhdL, label=fluid['name'])
 
-plt.plot(V, dhdL)
+plt.legend()
 plt.show()
 
 L4 = 10 / 3 # characteristic length (cm)
