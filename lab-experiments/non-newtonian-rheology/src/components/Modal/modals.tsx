220e425787ef20a34b956dd48da631fcbeec91b7
drew <drsm1370@colorado.edu>
2026-09-02 15:35:13 -0700

    modal

--- lab-experiments/non-newtonian-rheology/src/components/Modal/modals.tsx
+++ lab-experiments/non-newtonian-rheology/src/components/Modal/modals.tsx
@@ -3,12 +3,10 @@ import type { Component } from "solid-js";
 export const DirectionsText: Component = () => {
     return <>
     <p>
-        This digital experiment simulates the analysis of the Trinder reaction and
-        absorbance spectroscopy. Use the menu to choose which solutions to inject into
-        the cartridge and the concentration of sample solutions. Press inject to inject the liquids.
-        After injecting, press the green play button to mix the fluids and begin the reaction. You can
-        consider the reaction started when the fluids reach the bottom chamber.
-        Press the eye-dropper button to display the rgb values at the location of your cursor.
+        This digital experiment simulates a simplified rotary viscometer with several fluids.
+        Use the fluid selection menu to choose which fluid is tested. Press the play button to begin testing.
+        After a short animation, use the buttons on the motor control panel to adjust the rotation speed of the die head.
+        Hover over the spring scale to read the resulting force exerted on it.
         
         Scroll the mouse wheel to zoom and click and drag 
         to pan the view. Click the "Reset" button to reset the experiment. 
@@ -20,10 +18,8 @@ export const AboutText: Component = () => {
     return <p>
         This digital experiment was created in the Department of Chemical and Biological Engineering at University of Colorado Boulder 
         for <a href="learncheme.com">LearnChemE.com</a> by Drew Smith under the direction of Professor John L. Falconer. 
-        It was prepared with financial support from the National Science Foundation (DUE 2336987 and 2336988) and is based on a 
-        Glucose Analyzer experimental kit and
-        accompanying worksheet protocol developed with separate support
-        under NSF 1821578 led by Riley Fosbre at Washington State University. 
+        It was prepared with financial support from the National Science Foundation (DUE 2336987 and 2336988) and is based on laboratory
+        rotary viscometers.
         Address any questions or comments to LearnChemE@gmail.com.
     </p>
 }
