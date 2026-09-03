f2d313bd82983cd2c6fbbd5b407df3872a79d89f
drew <drsm1370@colorado.edu>
2026-09-03 13:54:34 -0700

    fixed magnifier tracking

--- lab-experiments/non-newtonian-rheology/src/components/Magnifier/MagnifierEmitter.tsx
+++ lab-experiments/non-newtonian-rheology/src/components/Magnifier/MagnifierEmitter.tsx
@@ -1,6 +1,8 @@
 import { createEffect, onCleanup, type Accessor, type JSX } from "solid-js";
 import { useMagnifier } from "./MagnifierContext";
-import { getSVGCoords } from "../../globals";
+import { getSVGCoords, resolveProperty } from "../../globals";
+
+type Vec2 = { x: number; y: number };
 
 // MagnifierEmitter component
 interface MagnifierEmitterProps {
@@ -10,12 +12,12 @@ interface MagnifierEmitterProps {
   scale: number;
   moveX?: boolean;
   moveY?: boolean;
-  offset?: { x: number; y: number };
+  offset?: Vec2 | Accessor<Vec2>;
 }
 
 export function MagnifierEmitter(props: MagnifierEmitterProps) {
   const magnifier = useMagnifier();
-  const offset = props.offset || { x: 0, y: 0 };
+  const offset = resolveProperty(props.offset, { x: 0, y: 0 });
 
   createEffect(() => {
     const ref = props.svgRef();
@@ -33,7 +35,9 @@ export function MagnifierEmitter(props: MagnifierEmitterProps) {
     const pt = getSVGCoords(e);
     const x = (props.moveX) ? pt.x : rect.x + rect.width/2;
     const y = (props.moveY) ? pt.y : rect.y + rect.height/2;
-    magnifier.setLensPosition({ x: x + offset.x, y: y + offset.y });
+    const newPos = { x: x + offset().x, y: y + offset().y };
+    console.log(offset())
+    magnifier.setLensPosition(newPos);
     magnifier.setActiveKey(props.emitterKey);
     magnifier.setScale(props.scale);
   };
